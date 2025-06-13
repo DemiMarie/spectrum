DemiMarie/spectrum

commit ae4597837487a17817d7b79a035abec362f1ad37

release/checks/networking: init
This test verifies that a VM on the system, going through the default
network provider, can reach an external server.

Signed-off-by: Alyssa Ross <hi@alyssa.is>

release/checks/networking/test.c
@@ -1,0 +1,335 @@
+// SPDX-License-Identifier: EUPL-1.2+
+// SPDX-FileCopyrightText: 2025 Alyssa Ross <hi@alyssa.is>
+
+#include <errno.h>
+#include <pthread.h>
+#include <sched.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <arpa/inet.h>
+#include <net/if.h>
+
+#include <sys/ioctl.h>
+#include <sys/prctl.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <sys/utsname.h>
+
+static void chld_handler(int)
+{
+	exit(EXIT_FAILURE);
+}
+
+static void exit_on_sigchld(void)
+{
+	struct sigaction sa = {
+		.sa_handler = chld_handler,
+		.sa_flags = SA_NOCLDSTOP,
+	};
+	if (!sigaction(SIGCHLD, &sa, nullptr))
+		return;
+
+	perror("sigaction");
+	exit(EXIT_FAILURE);
+}
+
+static void *drain(void *arg)
+{
+	int c;
+
+	while ((c = fgetc((FILE *)arg)) != EOF)
+		fputc(c, stderr);
+
+	if (ferror((FILE *)arg))
+		return (void *)(intptr_t)errno;
+
+	return nullptr;
+}
+
+static pthread_t start_drain(FILE *console)
+{
+	pthread_t thread;
+	int e;
+
+	if (!(e = pthread_create(&thread, nullptr, drain, console)))
+		return thread;
+
+	fprintf(stderr, "pthread_create: %s\n", strerror(e));
+	exit(EXIT_FAILURE);
+}
+
+static char *make_tmp_dir(void)
+{
+	char *dir, *run;
+	if (!(run = secure_getenv("XDG_RUNTIME_DIR"))) {
+		fputs("warning: XDG_RUNTIME_DIR unset\n", stderr);
+		run = "/tmp";
+	}
+	if (asprintf(&dir, "%s/spectrum-test.XXXXXX", run) == -1) {
+		perror("asprintf");
+		exit(EXIT_FAILURE);
+	}
+	if (!mkdtemp(dir)) {
+		perror("mkdtemp");
+		exit(EXIT_FAILURE);
+	}
+	return dir;
+}
+
+static int setup_server(void)
+{
+	int fd;
+	struct ifreq ifr;
+
+	struct sockaddr_in addr = {
+		.sin_family = AF_INET,
+		.sin_port = htons(1234),
+		.sin_addr = { .s_addr = htonl(INADDR_LOOPBACK) },
+	};
+
+	sprintf(ifr.ifr_name, "lo");
+
+	if ((fd = socket(AF_INET, SOCK_STREAM|SOCK_CLOEXEC, 0)) == -1) {
+		perror("socket");
+		exit(EXIT_FAILURE);
+	}
+
+	if (ioctl(fd, SIOCGIFFLAGS, &ifr) == -1) {
+		perror("SIOCGIFFLAGS");
+		exit(EXIT_FAILURE);
+	}
+
+	ifr.ifr_flags |= IFF_UP;
+	if (ioctl(fd, SIOCSIFFLAGS, &ifr) == -1) {
+		perror("SIOCSIFFLAGS");
+		exit(EXIT_FAILURE);
+	}
+
+	if (bind(fd, &addr, sizeof addr) == -1) {
+		perror("bind");
+		exit(EXIT_FAILURE);
+	}
+
+	if (listen(fd, 1) == -1) {
+		perror("listen");
+		exit(EXIT_FAILURE);
+	}
+
+	return fd;
+}
+
+static int setup_unix(const char *path)
+{
+	int r, fd;
+	struct sockaddr_un addr = { .sun_family = AF_UNIX };
+
+	r = snprintf(addr.sun_path, sizeof addr.sun_path, "%s", path);
+	if (r >= sizeof addr.sun_path) {
+		fputs("XDG_RUNTIME_DIR too long\n", stderr);
+		exit(EXIT_FAILURE);
+	}
+
+	if (r < 0) {
+		fputs("snprintf error\n", stderr);
+		exit(EXIT_FAILURE);
+	}
+
+	if ((fd = socket(AF_UNIX, SOCK_STREAM|SOCK_CLOEXEC, 0)) == -1) {
+		perror("socket");
+		exit(EXIT_FAILURE);
+	}
+
+	if (bind(fd, &addr, sizeof addr) == -1) {
+		perror("bind");
+		exit(EXIT_FAILURE);
+	}
+
+	if (listen(fd, 1) == -1) {
+		perror("listen");
+		exit(EXIT_FAILURE);
+	}
+
+	return fd;
+}
+
+static void wait_for_prompt(FILE *console)
+{
+	char *needle = "~ # ";
+	size_t i = 0;
+	int c;
+
+	fputs("waiting for character\n", stderr);
+
+	while ((c = fgetc(console)) != EOF) {
+		fputc(c, stderr);
+		i = c == needle[i] ? i + 1 : 0;
+		if (!needle[i])
+			return;
+	}
+
+	fputs("unexpected EOF from console\n", stderr);
+	exit(EXIT_FAILURE);
+}
+
+static FILE *start_qemu(const char *tmp_dir, const char *run_qemu,
+                        const char *efi, const char *img, const char *user_data)
+{
+	FILE *console;
+	struct utsname u;
+	int console_listener, console_conn;
+	char *arch, *args[] = {
+		(char *)run_qemu,
+		"-serial", nullptr,
+		"-drive", nullptr,
+		"-drive", nullptr,
+		"-drive", nullptr,
+		"-m", "4G",
+		"-nodefaults",
+		"-machine", "virtualization=on",
+		"-cpu", "max",
+		"-device", "virtio-keyboard",
+		"-device", "virtio-mouse",
+		"-device", "virtio-gpu",
+		"-netdev", "user,id=net0",
+		"-device", "e1000e,netdev=net0",
+		"-monitor", "vc",
+		"-vga", "none",
+		"-smbios", "type=11,value=io.systemd.stub.kernel-cmdline-extra=console=ttyS0",
+		nullptr,
+	};
+
+	if (!(arch = getenv("ARCH"))) {
+		uname(&u);
+		arch = u.machine;
+	}
+	if (strcmp(arch, "x86_64"))
+		args[sizeof args / sizeof *args - 3] = nullptr;
+
+	if (asprintf(&args[2], "unix:%s/console", tmp_dir) == -1) {
+		perror("asprintf");
+		exit(EXIT_FAILURE);
+	}
+
+	console_listener = setup_unix(args[2] + strlen("unix:"));
+
+	switch (fork()) {
+	case -1:
+		perror("fork");
+		exit(EXIT_FAILURE);
+	case 0:
+		if (prctl(PR_SET_PDEATHSIG, SIGTERM) == -1) {
+			perror("prctl");
+			exit(EXIT_FAILURE);
+		}
+
+		if (asprintf(&args[4], "file=%s,format=raw,if=pflash,readonly=true", efi) == -1 ||
+		    asprintf(&args[6], "file=%s,format=raw,if=virtio,readonly=true", img) == -1 ||
+		    asprintf(&args[8], "file=%s,format=raw,if=virtio,readonly=true", user_data) == -1) {
+			perror("asprintf");
+			exit(EXIT_FAILURE);
+		}
+
+		execv(run_qemu, args);
+		perror("execv");
+		exit(EXIT_FAILURE);
+	}
+
+	free(args[2]);
+
+	if ((console_conn = accept(console_listener, nullptr, nullptr)) == -1) {
+		perror("accept");
+		exit(EXIT_FAILURE);
+	}
+	if (!(console = fdopen(console_conn, "a+"))) {
+		perror("fdopen");
+		exit(EXIT_FAILURE);
+	}
+
+	fputs("waiting for console prompt\n", stderr);
+
+	wait_for_prompt(console);
+
+	return console;
+}
+
+static void expect_connection(int listener)
+{
+	int conn_fd;
+	FILE *conn;
+	char msg[7];
+	size_t len;
+
+	fputs("waiting for server connection\n", stderr);
+	if ((conn_fd = accept(listener, nullptr, nullptr)) == -1) {
+		perror("accept");
+		exit(EXIT_FAILURE);
+	}
+	fputs("accepted connection!\n", stderr);
+	if (!(conn = fdopen(conn_fd, "r"))) {
+		perror("fdopen(server connection)");
+		exit(EXIT_FAILURE);
+	}
+
+	len = fread(msg, 1, sizeof msg, conn);
+	if (len != 6 || memcmp("hello\n", msg, 6)) {
+		if (ferror(conn))
+			perror("fread(server connection)");
+		else
+			fprintf(stderr, "unexpected connection data: %.*s",
+			        (int)len, msg);
+		exit(EXIT_FAILURE);
+	}
+	fclose(conn);
+}
+
+int main(int argc, char *argv[])
+{
+	exit_on_sigchld();
+
+	if (argc != 5) {
+		fputs("Usage: test efi spectrum user_data\n", stderr);
+		exit(EXIT_FAILURE);
+	}
+
+	char *run_qemu = argv[1];
+	char *efi = argv[2];
+	char *img = argv[3];
+	char *user_data = argv[4];
+
+	if (strchr(efi, ',') || strchr(img, ',') || strchr(user_data, ',')) {
+		fputs("arguments contain commas\n", stderr);
+		exit(EXIT_FAILURE);
+	}
+
+	if (unshare(CLONE_NEWUSER|CLONE_NEWNET) == -1) {
+		perror("unshare");
+		exit(EXIT_FAILURE);
+	}
+
+	int server = setup_server();
+
+	FILE *console = start_qemu(make_tmp_dir(), run_qemu, efi, img,
+	                           user_data);
+
+	if (fputs("set -euxo pipefail\n"
+	          "mkdir /run/mnt\n"
+	          "mount \"$(findfs UUID=a7834806-2f82-4faf-8ac4-4f8fd8a474ca)\" /run/mnt\n"
+	          "s6-rc -bu change vmm-env\n"
+	          "vm-import user /run/mnt/vms\n"
+	          "vm-start user.vm\n"
+	          "tail -Fc +0 /run/log/current /run/user.vm.log\r\n",
+	          console) == EOF) {
+		fputs("error writing to console\n", stderr);
+		exit(EXIT_FAILURE);
+	}
+	if (fflush(console) == EOF) {
+		perror("fflush");
+		exit(EXIT_FAILURE);
+	}
+	start_drain(console);
+
+	expect_connection(server);
+}

release/checks/default.nix
@@ -16,6 +16,8 @@ import ../../lib/call-package.nix ({ callSpectrumPackage }:
 
   pkg-tests = callSpectrumPackage ./pkg-tests.nix {};
 
+  networking = callSpectrumPackage ./networking {};
+
   no-roothash = callSpectrumPackage ./no-roothash.nix {};
 
   reuse = callSpectrumPackage ./reuse.nix {};

release/checks/networking/default.nix
@@ -1,0 +1,62 @@
+# SPDX-License-Identifier: MIT
+# SPDX-FileCopyrightText: 2023, 2025 Alyssa Ross <hi@alyssa.is>
+
+import ../../../lib/call-package.nix (
+{ callSpectrumPackage, lib, stdenv, runCommand, runCommandCC, writeShellScript
+, clang-tools, e2fsprogs, tar2ext4, libressl, qemu_kvm
+}:
+
+let
+  live = callSpectrumPackage ../../live {};
+
+  vm = callSpectrumPackage ../../../vm/make-vm.nix {} {
+    providers.net = [ "user.netvm" ];
+    type = "nix";
+    run = writeShellScript "run" ''
+      set -x
+      while ! echo hello | ${libressl.nc}/bin/nc -N 10.0.2.2 1234; do :; done
+    '';
+  };
+
+  userData = runCommand "user-data.img" {
+    nativeBuildInputs = [ e2fsprogs tar2ext4 ];
+  } ''
+    tar --transform=s,^${vm},vms/vm, -Pcvf root.tar ${vm}
+    tar2ext4 -i root.tar -o $out
+    tune2fs -U a7834806-2f82-4faf-8ac4-4f8fd8a474ca $out
+  '';
+
+  cflags = [ "-std=c23" "-D_GNU_SOURCE" ];
+
+  test = runCommandCC "test" {} ''
+    $CC -o $out ${lib.escapeShellArgs cflags} ${./test.c}
+  '';
+
+  testScript = writeShellScript "spectrum-networking-test" ''
+    PATH=${lib.escapeShellArg (lib.makeBinPath [ qemu_kvm ])}:"$PATH"
+    exec ${test} \
+        ${../../../scripts/run-qemu.sh} \
+        ${qemu_kvm}/share/qemu/edk2-${stdenv.hostPlatform.qemuArch}-code.fd \
+        ${live} \
+        ${userData}
+  '';
+in
+
+runCommand "run-${testScript.name}" {
+  inherit testScript userData;
+
+  passthru.tests = {
+    clang-tidy = runCommand "run-${testScript.name}-clang-tidy" {
+      nativeBuildInputs = [ clang-tools ];
+    } ''
+      clang-tidy --config-file=${../../../.clang-tidy} --warnings-as-errors='*' \
+          ${lib.escapeShellArgs (map (flag: "--extra-arg=${flag}") cflags)} \
+          ${./test.c}
+      touch $out
+    '';
+  };
+} ''
+  export QEMU_SYSTEM='qemu-system-${stdenv.hostPlatform.qemuArch} -nographic'
+  $testScript
+  touch $out
+'') (_: {})

release/checks/pkg-tests.nix
@@ -9,6 +9,8 @@ import ../../lib/call-package.nix (
 
   lseek = lib.recurseIntoAttrs lseek.tests;
 
+  networking = lib.recurseIntoAttrs (callSpectrumPackage ./networking {}).tests;
+
   start-vmm = lib.recurseIntoAttrs start-vmm.tests;
 
   run-spectrum-vm = lib.recurseIntoAttrs

commit 42887a0331fd058ee0930fd5209fc3265565c3a8

release/checks/networking: include headers
<stdint.h> is where intptr_t is defined, and <unistd.h> is where
execv() is defined.  Include them directly instead of relying on some
other header bringing them in.

Fixes: ae45978 ("release/checks/networking: init")
Signed-off-by: Alyssa Ross <hi@alyssa.is>

release/checks/networking/test.c
@@ -5,9 +5,11 @@
 #include <pthread.h>
 #include <sched.h>
 #include <signal.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 #include <arpa/inet.h>
 #include <net/if.h>