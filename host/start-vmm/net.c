DemiMarie/spectrum

commit 68787f2a13123ba58d4fd809f325ada5d93970b2

tools/start-vmm: fix checking snprintf return
The standard doesn't specifically say that these functions return -1
on failure, just a negative value.

Fixes: 12e4cd0 ("host/rootfs: dynamically generate s6-rc services")
Signed-off-by: Alyssa Ross <hi@alyssa.is>

host/start-vmm/net.c
@@ -23,7 +23,7 @@ static int setup_tap(const char bridge_name[static 1],
 	int fd;
 
 	// We assume ≤16-bit pids.
-	if (snprintf(tap_name, IFNAMSIZ, "%s%d", tap_prefix, getpid()) == -1)
+	if (snprintf(tap_name, IFNAMSIZ, "%s%d", tap_prefix, getpid()) < -1)
 		return -1;
 	if ((fd = tap_open(tap_name, IFF_NO_PI|IFF_VNET_HDR|IFF_TUN_EXCL)) == -1)
 		goto out;
@@ -171,7 +171,7 @@ struct net_config net_setup(const struct vm_dir *router_vm_dir)
 	memcpy(r.mac, router_mac, 6);
 	r.mac[3] = 0x00;
 
-	if (snprintf(bridge_name, sizeof bridge_name, "br%d", pid) == -1)
+	if (snprintf(bridge_name, sizeof bridge_name, "br%d", pid) < -1)
 		return r;
 
 	if (bridge_add(bridge_name) == -1)

commit 37c8663fab86fdb202fece339ef7ac7177ffc201

host/start-vmm: fix checking snprintf return value
-1 is also an error.

Fixes: 68787f2 ("tools/start-vmm: fix checking snprintf return")
Signed-off-by: Alyssa Ross <hi@alyssa.is>

host/start-vmm/net.c
@@ -23,7 +23,7 @@ static int setup_tap(const char bridge_name[static 1],
 	int fd, e;
 
 	// We assume ≤16-bit pids.
-	if (snprintf(tap_name, IFNAMSIZ, "%s%d", tap_prefix, getpid()) < -1)
+	if (snprintf(tap_name, IFNAMSIZ, "%s%d", tap_prefix, getpid()) < 0)
 		return -1;
 	if ((fd = tap_open(tap_name, IFF_NO_PI|IFF_VNET_HDR|IFF_TUN_EXCL)) == -1)
 		goto out;
@@ -176,7 +176,7 @@ struct net_config net_setup(const struct vm_dir *router_vm_dir)
 	memcpy(r.mac, router_mac, 6);
 	r.mac[3] = 0x00;
 
-	if (snprintf(bridge_name, sizeof bridge_name, "br%d", pid) < -1)
+	if (snprintf(bridge_name, sizeof bridge_name, "br%d", pid) < 0)
 		return r;
 
 	if (bridge_add(bridge_name) == -1)