DemiMarie/spectrum

commit 048d4033736443efd7a53340e7555eb0852dea09

release/checks/integration: always be draining
If QEMU ever can't write to the console, it will block.  So if we're
ever writing to the console, while QEMU is trying to write, there's a
chance of deadlock.  The easiest way to avoid that is to make sure we
are always draining the console on its own thread.

The only reading from the console the tests currently do is waiting
for a prompt, so the console-draining thread is always scanning for a
prompt and will write into a pipe if one is found.  The pipe is
non-blocking to ensure the thread never blocks.

Fixes: ae45978 ("release/checks/networking: init")
Signed-off-by: Alyssa Ross <hi@alyssa.is>

release/checks/integration/lib.c
@@ -3,6 +3,8 @@
 
 #include "lib.h"
 
+#include <errno.h>
+#include <fcntl.h>
 #include <sched.h>
 #include <signal.h>
 #include <stdio.h>
@@ -10,6 +12,7 @@
 #include <string.h>
 #include <unistd.h>
 
+#include <sys/poll.h>
 #include <sys/prctl.h>
 #include <sys/socket.h>
 #include <sys/un.h>
@@ -85,29 +88,86 @@ static int setup_unix(const char *path)
 	return fd;
 }
 
-static void wait_for_prompt(FILE *console)
+struct console_thread_args {
+	FILE *console;
+	int prompt_event;
+};
+
+static void *console_thread(void *arg)
 {
+	struct console_thread_args *args = (struct console_thread_args *)arg;
+
 	char *needle = "~ # ";
 	size_t i = 0;
 	int c;
 
-	fputs("waiting for character\n", stderr);
-
-	while ((c = fgetc(console)) != EOF) {
+	while ((c = fgetc(args->console)) != EOF) {
 		fputc(c, stderr);
 		i = c == needle[i] ? i + 1 : 0;
-		if (!needle[i])
-			return;
+
+		if (!needle[i]) {
+			i = 0;
+			if (write(args->prompt_event, "\n", 1) == -1 && errno != EAGAIN) {
+				perror("write");
+				exit(EXIT_FAILURE);
+			}
+		}
 	}
 
 	fputs("unexpected EOF from console\n", stderr);
 	exit(EXIT_FAILURE);
 }
 
-FILE *start_qemu(struct config c)
+static int start_console_thread(FILE *console, pthread_t *thread)
 {
-	FILE *console;
+	int e, prompt_event[2];
+	struct console_thread_args *args = malloc(sizeof(*args));
+
+	if (!args) {
+		perror("malloc");
+		exit(EXIT_FAILURE);
+	}
+
+	if (pipe2(prompt_event, O_CLOEXEC|O_NONBLOCK) == -1) {
+		perror("pipe");
+		exit(EXIT_FAILURE);
+	}
+
+	args->console = console;
+	args->prompt_event = prompt_event[1];
+
+	if ((e = pthread_create(thread, nullptr, console_thread, args))) {
+		fprintf(stderr, "pthread_create: %s\n", strerror(e));
+		exit(EXIT_FAILURE);
+	}
+
+	return prompt_event[0];
+}
+
+static void wait_for_prompt(int prompt_event)
+{
+	char c;
+	struct pollfd pollfd = { .fd = prompt_event, .events = POLLIN };
+	if (poll(&pollfd, 1, -1) == -1) {
+		perror("poll");
+		exit(EXIT_FAILURE);
+	}
+	if (pollfd.revents != POLLIN) {
+		fprintf(stderr, "unexpected poll events from prompt event: %hx\n", pollfd.revents);
+		exit(EXIT_FAILURE);
+	}
+	while (read(prompt_event, &c, 1) != -1);
+	if (errno != EAGAIN && errno != EINTR) {
+		perror("read prompt event");
+		exit(EXIT_FAILURE);
+	}
+}
+
+struct vm start_qemu(struct config c)
+{
+	struct vm r;
 	struct utsname u;
+	FILE *console_reader;
 	int console_listener, console_conn;
 	char *arch, *args[] = {
 		(char *)c.run_qemu,
@@ -171,16 +231,25 @@ FILE *start_qemu(struct config c)
 		perror("accept");
 		exit(EXIT_FAILURE);
 	}
-	if (!(console = fdopen(console_conn, "a+"))) {
+
+	if (!(console_reader = fdopen(console_conn, "r"))) {
 		perror("fdopen");
 		exit(EXIT_FAILURE);
 	}
 
-	fputs("waiting for console prompt\n", stderr);
+	if ((console_conn = fcntl(console_conn, F_DUPFD_CLOEXEC, 0)) == -1) {
+		perror("fcntl(F_DUPFD_CLOEXEC)");
+		exit(EXIT_FAILURE);
+	}
 
-	wait_for_prompt(console);
+	if (!(r.console = fdopen(console_conn, "a"))) {
+		perror("fdopen");
+		exit(EXIT_FAILURE);
+	}
 
-	return console;
+	r.prompt_event = start_console_thread(console_reader, &r.console_thread);
+	wait_for_prompt(r.prompt_event);
+	return r;
 }
 
 int main(int argc, char *argv[])

release/checks/integration/lib.h
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: EUPL-1.2+
 // SPDX-FileCopyrightText: 2025 Alyssa Ross <hi@alyssa.is>
 
+#include <pthread.h>
 #include <stdio.h>
 
 struct config {
@@ -17,4 +18,10 @@ struct config {
 
 extern void test(struct config);
 
-FILE *start_qemu(struct config config);
+struct vm {
+	pthread_t console_thread;
+	FILE *console;
+	int prompt_event;
+};
+
+struct vm start_qemu(struct config config);

release/checks/integration/networking.c
@@ -3,9 +3,6 @@
 
 #include "lib.h"
 
-#include <errno.h>
-#include <pthread.h>
-#include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
 
@@ -13,31 +10,6 @@
 #include <net/if.h>
 
 #include <sys/ioctl.h>
-
-static void *drain(void *arg)
-{
-	int c;
-
-	while ((c = fgetc((FILE *)arg)) != EOF)
-		fputc(c, stderr);
-
-	if (ferror((FILE *)arg))
-		return (void *)(intptr_t)errno;
-
-	return nullptr;
-}
-
-static pthread_t start_drain(FILE *console)
-{
-	pthread_t thread;
-	int e;
-
-	if (!(e = pthread_create(&thread, nullptr, drain, console)))
-		return thread;
-
-	fprintf(stderr, "pthread_create: %s\n", strerror(e));
-	exit(EXIT_FAILURE);
-}
 
 static int setup_server(void)
 {
@@ -115,7 +87,7 @@ void test(struct config c)
 {
 	int server = setup_server();
 
-	FILE *console = start_qemu(c);
+	struct vm vm = start_qemu(c);
 
 	if (fputs("set -euxo pipefail\n"
 	          "mkdir /run/mnt\n"
@@ -124,15 +96,14 @@ void test(struct config c)
 	          "vm-import user /run/mnt/vms\n"
 	          "vm-start user.nc\n"
 	          "tail -Fc +0 /run/log/current /run/*.log\n",
-	          console) == EOF) {
+	          vm.console) == EOF) {
 		fputs("error writing to console\n", stderr);
 		exit(EXIT_FAILURE);
 	}
-	if (fflush(console) == EOF) {
+	if (fflush(vm.console) == EOF) {
 		perror("fflush");
 		exit(EXIT_FAILURE);
 	}
-	start_drain(console);
 
 	expect_connection(server);
 }

commit 3991703c7c1599bb45e14ea969efac9cb970f743

release/checks/integration: unlock console reads
When the process exits (in this case because the main thread has
reached the end of the test), libc will clean up all open streams,
which requires locking them.  If the reader thread is in fgetc at that
point, there will be a deadlock because the reader thread will hold
the lock until it receives a character, but it's likely that no more
characters will ever be received at that point.

To avoid this, don't lock in the reader thread.  Since the stream is
only ever read from on the reader thread, this should be fine.

Fixes: 048d403 ("release/checks/integration: always be draining")
Signed-off-by: Alyssa Ross <hi@alyssa.is>

release/checks/integration/lib.c
@@ -101,7 +101,7 @@ static void *console_thread(void *arg)
 	size_t i = 0;
 	int c;
 
-	while ((c = fgetc(args->console)) != EOF) {
+	while ((c = getc_unlocked(args->console)) != EOF) {
 		fputc(c, stderr);
 		i = c == needle[i] ? i + 1 : 0;
 