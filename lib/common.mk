DemiMarie/spectrum

commit c1e1449b2173913f08828bafc62dc112b2534acc

Remake root filesystems when packages change
This is the most common case where make clean is required, and it's
easy to forget and end up with a broken image.  We could probably do
the same for other environment variables coming from Nix if we wanted.

Signed-off-by: Alyssa Ross <hi@alyssa.is>

lib/common.mk
@@ -1,5 +1,5 @@
 # SPDX-License-Identifier: EUPL-1.2+
-# SPDX-FileCopyrightText: 2021, 2023 Alyssa Ross <hi@alyssa.is>
+# SPDX-FileCopyrightText: 2021, 2023, 2025 Alyssa Ross <hi@alyssa.is>
 
 BACKGROUND = background
 CPIO = cpio
@@ -17,3 +17,9 @@ TRUNCATE = truncate
 UKIFY = ukify
 VERITYSETUP = veritysetup
 VIRTIOFSD = virtiofsd
+
+PACKAGES_FILE != \
+	if ! [ -e build/packages.txt ] || ! cmp -s $$PACKAGES build/packages.txt; then \
+		mkdir -p build && cp -f $$PACKAGES build/packages.txt ;\
+	fi ;\
+	echo build/packages.txt

host/rootfs/Makefile
@@ -110,9 +110,9 @@ LINKS = \
 
 BUILD_FILES = build/etc/s6-rc
 
-$(dest): ../../scripts/make-erofs.sh $(FILES) $(BUILD_FILES) build/empty build/fifo
+$(dest): ../../scripts/make-erofs.sh $(PACKAGES_FILE) $(FILES) $(BUILD_FILES) build/empty build/fifo
 	( \
-	    cat $$PACKAGES ;\
+	    cat $(PACKAGES_FILE) ;\
 	    for file in $(FILES) $(LINKS); do printf '%s\n%s\n' $$file $$file; done ;\
 	    for file in $(BUILD_FILES); do printf '%s\n%s\n' $$file $${file#build/}; done ;\
 	    printf 'build/empty\n%s\n' $(DIRS) ;\

img/app/Makefile
@@ -71,9 +71,9 @@ build/fifo:
 build/empty:
 	mkdir -p $@
 
-build/rootfs.erofs: ../../scripts/make-erofs.sh $(VM_FILES) $(VM_BUILD_FILES) build/empty build/fifo
+build/rootfs.erofs: ../../scripts/make-erofs.sh $(PACKAGES_FILE) $(VM_FILES) $(VM_BUILD_FILES) build/empty build/fifo
 	( \
-	    cat $$PACKAGES ;\
+	    cat $(PACKAGES_FILE) ;\
 	    for file in $(VM_FILES) $(VM_LINKS); do printf '%s\n%s\n' $$file $$file; done ;\
 	    for file in $(VM_BUILD_FILES); do printf '%s\n%s\n' $$file $${file#build/}; done ;\
 	    printf 'build/empty\n%s\n' $(VM_DIRS) ;\

vm/sys/net/Makefile
@@ -50,9 +50,9 @@ VM_BUILD_FILES = build/etc/s6-rc
 build/empty:
 	mkdir -p $@
 
-build/rootfs.erofs: ../../../scripts/make-erofs.sh $(VM_FILES) $(VM_BUILD_FILES) build/empty
+build/rootfs.erofs: ../../../scripts/make-erofs.sh $(PACKAGES_FILE) $(VM_FILES) $(VM_BUILD_FILES) build/empty
 	( \
-	    cat $$PACKAGES ;\
+	    cat $(PACKAGES_FILE) ;\
 	    for file in $(VM_FILES) $(VM_LINKS); do printf '%s\n%s\n' $$file $$file; done ;\
 	    for file in $(VM_BUILD_FILES); do printf '%s\n%s\n' $$file $${file#build/}; done ;\
 	    printf 'build/empty\n%s\n' $(VM_DIRS) ;\

commit 586577f3015397afacd83bc185454f4cc3c8028f

lib/common.mk: fix PACKAGES_FILE with no PACKAGES
release/live doesn't export a PACKAGES variable (and so won't use
PACKAGES_FILE either).  Check it's defined before doing anything else,
or cmp will fail.

Fixes: c1e1449 ("Remake root filesystems when packages change")
Signed-off-by: Alyssa Ross <hi@alyssa.is>

lib/common.mk
@@ -19,7 +19,9 @@ VERITYSETUP = veritysetup
 VIRTIOFSD = virtiofsd
 
 PACKAGES_FILE != \
-	if ! [ -e build/packages.txt ] || ! cmp -s $$PACKAGES build/packages.txt; then \
-		mkdir -p build && cp -f $$PACKAGES build/packages.txt ;\
-	fi ;\
-	echo build/packages.txt
+	if [ -n "$$PACKAGES" ]; then \
+	    if ! [ -e build/packages.txt ] || ! cmp -s "$$PACKAGES" build/packages.txt; then \
+	        mkdir -p build && cp -f "$$PACKAGES" build/packages.txt ;\
+	    fi ;\
+	    echo build/packages.txt ;\
+	fi