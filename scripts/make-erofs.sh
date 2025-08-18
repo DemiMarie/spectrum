a35354d7c6dd25c17ed48a3da8228fbf8c3904e7
Demi Marie Obenour <demiobenour@gmail.com>
2025-08-18 00:03:00 -0400

    scripts/make-erofs.sh: Ensure that / is world-readable
    Previously it had 0700 permissions, which was hidden because everything
    ran as root anyway.  However, dbus-broker fails to start in this case
    because it always drops privileges.  Also set umask to 0022 to ensure
    that the permissions of other directories are correct.
    
    Signed-off-by: Demi Marie Obenour <demiobenour@gmail.com>

--- scripts/make-erofs.sh
+++ scripts/make-erofs.sh
@@ -1,6 +1,7 @@
 #!/bin/sh -eu
 #
 # SPDX-FileCopyrightText: 2023-2024 Alyssa Ross <hi@alyssa.is>
+# SPDX-FileCopyrightText: 2025 Demi Marie Obenour <demiobenour@gmail.com>
 # SPDX-License-Identifier: EUPL-1.2+
 #
 # FIXME: It would be nice to replace this script with a program that
@@ -8,6 +9,7 @@
 #        single directory structure, and could generate an EROFS image
 #        based on source:dest mappings directly.
 
+umask 0022 # for permissions
 ex_usage() {
 	echo "Usage: make-erofs.sh [options]... img < srcdest.txt" >&2
 	exit 1
@@ -18,8 +20,12 @@ if [ -z "${img-}" ]; then
 	ex_usage
 fi
 
-root="$(mktemp -d -- "$img.tmp.XXXXXXXXXX")"
-trap 'chmod -R +w -- "$root" && rm -rf -- "$root"' EXIT
+superroot="$(mktemp -d -- "$img.tmp.XXXXXXXXXX")"
+trap 'chmod -R +w -- "$root" && rm -rf -- "$superroot"' EXIT
+# $superroot has 0700 permissions, so create a subdirectory
+# with correct (0755) permissions and do all work there.
+root=$superroot/real_root
+mkdir -- "$root"
 
 while read -r arg1; do
 	read -r arg2 || ex_usage
