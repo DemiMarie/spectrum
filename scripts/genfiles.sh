b65668a5d43c216593fbfd1867569cadd62049f1
Demi Marie Obenour <demiobenour@gmail.com>
2025-08-31 19:42:46 -0400

    Generate file lists from a script
    Right now, the makefiles in host/rootfs, vm/sys/net, and img/app have
    manually-maintained lists of files and symlinks.  These duplicate the
    information in the git repository and can easily get out of sync or
    cause unnecessary merge conflicts.  Fix all of these issues by having
    the git repository be the source of truth, and using a script to
    generate the file lists.  Developers can regenerate the lists before
    every commit, or even add a git hook to do that.
    
    Signed-off-by: Demi Marie Obenour <demiobenour@gmail.com>

---
+++ scripts/genfiles.sh
@@ -0,0 +1,26 @@
+#!/bin/sh --
+# SPDX-License-Identifier: EUPL-1.2+
+# SPDX-FileCopyrightText: 2025 Demi Marie Obenour <demiobenour@gmail.com>
+set -euo pipefail
+export LC_ALL=C LANGUAGE=C
+dir=$(git rev-parse --show-toplevel)
+for i in host/rootfs img/app vm/sys/net; do
+    output_file=$i/file-list.mk
+    {
+        git -C "$i" -c core.quotePath=true ls-files $'--format=%(objectmode)\t%(path)' -- image |
+        sort -t $'\t' -k 2
+    } |
+    awk -f scripts/genfiles.awk > "$output_file.tmp"
+    if [ -f "$output_file" ]; then
+        # Avoid changing output file if it is up to date, as that
+        # would cause unnecessary rebuilds.
+        if cmp -s -- "$output_file.tmp" "$output_file"; then
+            rm -- "$output_file.tmp"
+            continue
+        else
+            astatus=$?
+            if [ "$astatus" != 1 ]; then exit "$astatus"; fi
+        fi
+    fi
+    mv -- "$output_file.tmp" "$output_file"
+done
