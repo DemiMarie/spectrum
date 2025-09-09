cbb10b15f36a7ac35400e226b284b1cb091cb3c9
Demi Marie Obenour <demiobenour@gmail.com>
2025-09-08 23:45:16 -0400

    Prefer definition-before-use in Nix
    Nix does not require this, but it is easier to read.
    
    No functional change intended.
    
    Signed-off-by: Demi Marie Obenour <demiobenour@gmail.com>
    Message-ID: <20250908-nix-cleanup-v1-1-1a0e49e5ec4e@gmail.com>
    Reviewed-by: Alyssa Ross <hi@alyssa.is>
    Signed-off-by: Alyssa Ross <hi@alyssa.is>

--- img/app/default.nix
+++ img/app/default.nix
@@ -12,6 +12,42 @@ pkgsStatic.callPackage (
 }:
 
 let
+  kernelTarget =
+    if stdenvNoCC.hostPlatform.isx86 then
+      # vmlinux.bin is the stripped version of vmlinux.
+      # Confusingly, compressed/vmlinux.bin is the stripped version of
+      # the top-level vmlinux target, while the top-level vmlinux.bin
+      # is the stripped version of compressed/vmlinux.  So we use
+      # compressed/vmlinux.bin, since we want a stripped version of
+      # the kernel that *hasn't* been built to be compressed.  Weird!
+      "compressed/vmlinux.bin"
+    else
+      stdenvNoCC.hostPlatform.linux-kernel.target;
+
+  kernel = (linux_latest.override {
+    structuredExtraConfig = with lib.kernel; {
+      DRM_FBDEV_EMULATION = lib.mkForce no;
+      EROFS_FS = yes;
+      FONTS = lib.mkForce unset;
+      FONT_8x8 = lib.mkForce unset;
+      FONT_TER16x32 = lib.mkForce unset;
+      FRAMEBUFFER_CONSOLE = lib.mkForce unset;
+      FRAMEBUFFER_CONSOLE_DEFERRED_TAKEOVER = lib.mkForce unset;
+      FRAMEBUFFER_CONSOLE_DETECT_PRIMARY = lib.mkForce unset;
+      FRAMEBUFFER_CONSOLE_ROTATION = lib.mkForce unset;
+      RC_CORE = lib.mkForce unset;
+      VIRTIO = yes;
+      VIRTIO_BLK = yes;
+      VIRTIO_CONSOLE = yes;
+      VIRTIO_PCI = yes;
+      VT = no;
+    };
+  }).overrideAttrs ({ installFlags ? [], ... }: {
+    installFlags = installFlags ++ [
+      "KBUILD_IMAGE=$(boot)/${kernelTarget}"
+    ];
+  });
+
   appimageFhsenv = (buildFHSEnv (appimageTools.defaultFhsEnvArgs // {
     name = "vm-fhs-env";
     targetPkgs = pkgs: appimageTools.defaultFhsEnvArgs.targetPkgs pkgs ++ [
@@ -53,50 +89,12 @@ let
       pkgs.wireplumber
     ];
   })).fhsenv;
-in
 
-let
   packagesSysroot = runCommand "packages-sysroot" {} ''
     mkdir -p $out/etc/ssl/certs
     ln -s ${appimageFhsenv}/{lib64,usr} ${kernel}/lib $out
     ln -s ${cacert}/etc/ssl/certs/* $out/etc/ssl/certs
   '';
-
-  kernelTarget =
-    if stdenvNoCC.hostPlatform.isx86 then
-      # vmlinux.bin is the stripped version of vmlinux.
-      # Confusingly, compressed/vmlinux.bin is the stripped version of
-      # the top-level vmlinux target, while the top-level vmlinux.bin
-      # is the stripped version of compressed/vmlinux.  So we use
-      # compressed/vmlinux.bin, since we want a stripped version of
-      # the kernel that *hasn't* been built to be compressed.  Weird!
-      "compressed/vmlinux.bin"
-    else
-      stdenvNoCC.hostPlatform.linux-kernel.target;
-
-  kernel = (linux_latest.override {
-    structuredExtraConfig = with lib.kernel; {
-      DRM_FBDEV_EMULATION = lib.mkForce no;
-      EROFS_FS = yes;
-      FONTS = lib.mkForce unset;
-      FONT_8x8 = lib.mkForce unset;
-      FONT_TER16x32 = lib.mkForce unset;
-      FRAMEBUFFER_CONSOLE = lib.mkForce unset;
-      FRAMEBUFFER_CONSOLE_DEFERRED_TAKEOVER = lib.mkForce unset;
-      FRAMEBUFFER_CONSOLE_DETECT_PRIMARY = lib.mkForce unset;
-      FRAMEBUFFER_CONSOLE_ROTATION = lib.mkForce unset;
-      RC_CORE = lib.mkForce unset;
-      VIRTIO = yes;
-      VIRTIO_BLK = yes;
-      VIRTIO_CONSOLE = yes;
-      VIRTIO_PCI = yes;
-      VT = no;
-    };
-  }).overrideAttrs ({ installFlags ? [], ... }: {
-    installFlags = installFlags ++ [
-      "KBUILD_IMAGE=$(boot)/${kernelTarget}"
-    ];
-  });
 in
 
 stdenvNoCC.mkDerivation {
--- vm/sys/net/default.nix
+++ vm/sys/net/default.nix
@@ -14,6 +14,42 @@ let
   inherit (lib) concatMapStringsSep;
   inherit (nixosAllHardware.config.hardware) firmware;
 
+  kernelTarget =
+    if stdenvNoCC.hostPlatform.isx86 then
+      # vmlinux.bin is the stripped version of vmlinux.
+      # Confusingly, compressed/vmlinux.bin is the stripped version of
+      # the top-level vmlinux target, while the top-level vmlinux.bin
+      # is the stripped version of compressed/vmlinux.  So we use
+      # compressed/vmlinux.bin, since we want a stripped version of
+      # the kernel that *hasn't* been built to be compressed.  Weird!
+      "compressed/vmlinux.bin"
+    else
+      stdenvNoCC.hostPlatform.linux-kernel.target;
+
+  kernel = (linux_latest.override {
+    structuredExtraConfig = with lib.kernel; {
+      DRM_FBDEV_EMULATION = lib.mkForce no;
+      EROFS_FS = yes;
+      FONTS = lib.mkForce unset;
+      FONT_8x8 = lib.mkForce unset;
+      FONT_TER16x32 = lib.mkForce unset;
+      FRAMEBUFFER_CONSOLE = lib.mkForce unset;
+      FRAMEBUFFER_CONSOLE_DEFERRED_TAKEOVER = lib.mkForce unset;
+      FRAMEBUFFER_CONSOLE_DETECT_PRIMARY = lib.mkForce unset;
+      FRAMEBUFFER_CONSOLE_ROTATION = lib.mkForce unset;
+      RC_CORE = lib.mkForce unset;
+      VIRTIO = yes;
+      VIRTIO_BLK = yes;
+      VIRTIO_CONSOLE = yes;
+      VIRTIO_PCI = yes;
+      VT = no;
+    };
+  }).overrideAttrs ({ installFlags ? [], ... }: {
+    installFlags = installFlags ++ [
+      "KBUILD_IMAGE=$(boot)/${kernelTarget}"
+    ];
+  });
+
   connman = connmanMinimal;
 
   packages = [
@@ -57,42 +93,6 @@ let
     imports = [ (modulesPath + "/profiles/all-hardware.nix") ];
 
     system.stateVersion = lib.trivial.release;
-  });
-
-  kernelTarget =
-    if stdenvNoCC.hostPlatform.isx86 then
-      # vmlinux.bin is the stripped version of vmlinux.
-      # Confusingly, compressed/vmlinux.bin is the stripped version of
-      # the top-level vmlinux target, while the top-level vmlinux.bin
-      # is the stripped version of compressed/vmlinux.  So we use
-      # compressed/vmlinux.bin, since we want a stripped version of
-      # the kernel that *hasn't* been built to be compressed.  Weird!
-      "compressed/vmlinux.bin"
-    else
-      stdenvNoCC.hostPlatform.linux-kernel.target;
-
-  kernel = (linux_latest.override {
-    structuredExtraConfig = with lib.kernel; {
-      DRM_FBDEV_EMULATION = lib.mkForce no;
-      EROFS_FS = yes;
-      FONTS = lib.mkForce unset;
-      FONT_8x8 = lib.mkForce unset;
-      FONT_TER16x32 = lib.mkForce unset;
-      FRAMEBUFFER_CONSOLE = lib.mkForce unset;
-      FRAMEBUFFER_CONSOLE_DEFERRED_TAKEOVER = lib.mkForce unset;
-      FRAMEBUFFER_CONSOLE_DETECT_PRIMARY = lib.mkForce unset;
-      FRAMEBUFFER_CONSOLE_ROTATION = lib.mkForce unset;
-      RC_CORE = lib.mkForce unset;
-      VIRTIO = yes;
-      VIRTIO_BLK = yes;
-      VIRTIO_CONSOLE = yes;
-      VIRTIO_PCI = yes;
-      VT = no;
-    };
-  }).overrideAttrs ({ installFlags ? [], ... }: {
-    installFlags = installFlags ++ [
-      "KBUILD_IMAGE=$(boot)/${kernelTarget}"
-    ];
   });
 in
 
