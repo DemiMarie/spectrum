DemiMarie/spectrum

commit a14dc133b3436a10ff92aadd522dadd094e26400

tools/start-vmm: drop extra parameter from extern
Passing an extra parameter wasn't harmful as far as I know, but not
good!

Fixes: cc31a6d ("host/rootfs: connect both ends of providers")
Reported-by: Someone <else+nixpkgs@someonex.net>
Link: https://matrix.to/#/!xSysqhzbOZImdvGpix:fairydust.space/$meOdvPu_BfH3pNl12Ba7tloN0JMTXGPtP7mstuRG3Bs?via=fairydust.space&via=matrix.org&via=dataaturservice.se
Signed-off-by: Alyssa Ross <hi@alyssa.is>

tools/start-vmm/net.rs
@@ -4,7 +4,6 @@
 use std::borrow::Cow;
 use std::ffi::{c_char, c_int};
 use std::fmt::{self, Display, Formatter};
-use std::path::Path;
 
 use miniserde::ser::Fragment;
 use miniserde::Serialize;
@@ -44,7 +43,7 @@ extern "C" {
     // SAFETY: &Path is sized, so it's okay to pass a reference to it
     // to C, as long as it's opaque to C.
     #[allow(improper_ctypes)]
-    pub fn net_setup(name: *const c_char, len: c_int, provider_vm_dir: &&Path) -> NetConfigC;
+    pub fn net_setup(name: *const c_char, len: c_int) -> NetConfigC;
 }
 
 #[cfg(test)]

tools/start-vmm/lib.rs
@@ -41,8 +41,9 @@ pub fn vm_config(vm_dir: &Path) -> Result<VmConfig, String> {
         return Err(format!("VM name may not contain a colon: {vm_name:?}"));
     }
 
-    let config_dir = vm_dir.join("config");
+    let name_bytes = vm_name.as_bytes();
 
+    let config_dir = vm_dir.join("config");
     let blk_dir = config_dir.join("blk");
     let kernel_path = config_dir.join("vmlinux");
     let net_providers_dir = config_dir.join("providers/net");
@@ -93,46 +94,24 @@ pub fn vm_config(vm_dir: &Path) -> Result<VmConfig, String> {
             shared: true,
         },
         net: match net_providers_dir.read_dir() {
-            Ok(entries) => entries
-                .into_iter()
-                .map(|result| {
-                    let provider_name = result
-                        .map_err(|e| format!("examining directory entry: {e}"))?
-                        .file_name()
-                        .into_string()
-                        .map_err(|name| format!("provider name {name:?} is not UTF-8"))?;
+            Ok(_) => {
+                // SAFETY: we check the result.
+                let net = unsafe {
+                    net_setup(
+                        name_bytes.as_ptr() as _,
+                        name_bytes
+                            .len()
+                            .try_into()
+                            .map_err(|e| format!("VM name too long: {e}"))?,
+                    )
+                };
+                if net.fd == -1 {
+                    let e = io::Error::last_os_error();
+                    return Err(format!("setting up networking failed: {e}"));
+                }
 
-                    let provider_dir = vm_dir
-                        .parent()
-                        .unwrap()
-                        .parent()
-                        .unwrap()
-                        .join("by-name")
-                        .join(provider_name);
-
-                    let name_bytes = vm_name.as_bytes();
-                    // SAFETY: we check the result.
-                    let net = unsafe {
-                        net_setup(
-                            name_bytes.as_ptr() as _,
-                            name_bytes
-                                .len()
-                                .try_into()
-                                .map_err(|e| format!("VM name too long: {e}"))?,
-                            &provider_dir.as_path(),
-                        )
-                    };
-                    if net.fd == -1 {
-                        let e = io::Error::last_os_error();
-                        return Err(format!("setting up networking failed: {e}"));
-                    }
-
-                    Ok(net.try_into().unwrap())
-                })
-                // TODO: to support multiple net providers, we'll need
-                // a better naming scheme for tap and bridge devices.
-                .take(1)
-                .collect::<Result<_, _>>()?,
+                vec![net.try_into().unwrap()]
+            }
             Err(e) if e.kind() == ErrorKind::NotFound => Default::default(),
             Err(e) => return Err(format!("reading directory {net_providers_dir:?}: {e}")),
         },

commit a96505e23ef06c36cd59077164411dfd2f7c25f9

tools/start-vmm: don't disable improper_ctypes
The code didn't use improper ctypes.

Signed-off-by: Demi Marie Obenour <demiobenour@gmail.com>
Message-ID: <20250918-no-improper-ctypes-v1-1-20b17c8ef946@gmail.com>
Fixes: a14dc13 ("tools/start-vmm: drop extra parameter from extern")
Reviewed-by: Alyssa Ross <hi@alyssa.is>
Signed-off-by: Alyssa Ross <hi@alyssa.is>

tools/start-vmm/net.rs
@@ -40,9 +40,6 @@ extern "C" {
     /// # Safety
     ///
     /// The rest of the result is only valid if the returned fd is not -1.
-    // SAFETY: &Path is sized, so it's okay to pass a reference to it
-    // to C, as long as it's opaque to C.
-    #[allow(improper_ctypes)]
     pub fn net_setup(name: *const c_char, len: c_int) -> NetConfigC;
 }
 