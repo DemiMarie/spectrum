cb5c648fb98b39b70da620882d957d58626fd301
Demi Marie Obenour <demiobenour@gmail.com>
2025-10-31 23:22:52 -0400

    release: Create directory with system update
    Whenever a release is made, create a directory with the release files to
    be used for an update.  After its SHA256SSUMS file is signed, the file
    is ready to be uploaded to a webserver for users to update from.
    
    Signed-off-by: Demi Marie Obenour <demiobenour@gmail.com>

---
+++ release/update.nix
@@ -0,0 +1,37 @@
+# SPDX-License-Identifier: MIT
+# SPDX-FileCopyrightText: 2021-2024 Alyssa Ross <hi@alyssa.is>
+# SPDX-FileCopyrightText: 2025 Demi Marie Obenour <demiobenour@gmail.com>
+
+import ../lib/call-package.nix (
+{ callSpectrumPackage, config, runCommand, stdenv }:
+
+let
+  efi = import ../host/efi.nix {};
+in
+runCommand "spectrum-update-directory" {
+  __structuredAttrs = true;
+  unsafeDiscardReferences = { out = true; };
+  dontFixup = true;
+  env = { VERSION = config.version; };
+} ''
+  # One would expect that this is enabled already but it is not.
+  set -euo pipefail
+  mkdir -- "$out"
+  cd -- "$out"
+  read -r roothash < ${efi.rootfs}/rootfs.verity.roothash
+  if ! [[ "$roothash" =~ ^[0-9a-f]{64}$ ]]; then
+    printf 'Internal error: bad root hash %q\n' "$roothash"
+    exit 1
+  fi
+  if ! [[ "$VERSION" =~ ^[[:alnum:]+.^~-]+$ ]]; then
+    printf 'Unsupported version %q: only alphanumeric characters, "+", ".", "^", "~", and "-" allowed.\n' "$VERSION" >&2
+    exit 1
+  fi
+  cp -- ${efi} "Spectrum_$VERSION.efi"
+  cp -- ${efi.rootfs}/rootfs.verity.superblock "Spectrum_''${VERSION}_''${roothash:32:32}.verity"
+  cp -- ${efi.rootfs}/rootfs "Spectrum_''${VERSION}_''${roothash:0:32}.root"
+  sha256sum -b "Spectrum_$VERSION.efi" \
+    "Spectrum_''${VERSION}_''${roothash:32:32}.verity" \
+    "Spectrum_''${VERSION}_''${roothash:0:32}.root" > SHA256SUMS
+  ''
+) (_: {})
