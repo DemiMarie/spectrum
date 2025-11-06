DemiMarie/spectrum

commit 111b58b830373447a824f2122f29c47938d14471

Remove unused packages

host/initramfs/shell.nix
@@ -17,7 +17,7 @@ with pkgs;
 
 initramfs.overrideAttrs ({ nativeBuildInputs ? [], ... }: {
   nativeBuildInputs = nativeBuildInputs ++ [
-    cryptsetup jq qemu_kvm util-linux
+    cryptsetup qemu_kvm util-linux
   ];
 
   EXT_FS = extfs;

host/rootfs/default.nix
@@ -4,8 +4,8 @@
 { pkgs ? import <nixpkgs> {} }: pkgs.pkgsStatic.callPackage (
 
 { lib, stdenv, runCommand, writeReferencesToFile, s6-rc, tar2ext4
-, busybox, cloud-hypervisor, cryptsetup, curl, execline, jq, mdevd, mktuntap, s6
-, s6-linux-utils, s6-portable-utils, screen, socat, util-linuxMinimal, xorg
+, busybox, cloud-hypervisor, cryptsetup, execline, jq, mdevd, s6
+, s6-linux-utils, s6-portable-utils, socat, util-linuxMinimal, xorg
 }:
 
 let
@@ -20,8 +20,8 @@ let
   foot = pkgsGui.foot.override { allowPgo = false; };
 
   packages = [
-    cloud-hypervisor curl execline jq mdevd mktuntap s6 s6-linux-utils
-    s6-portable-utils s6-rc screen socat start-vm
+    cloud-hypervisor execline jq mdevd s6 s6-linux-utils
+    s6-portable-utils s6-rc socat start-vm
 
     (cryptsetup.override {
       programs = {

commit 22e216712322cdfb85094bbd27ff34c4366fad41

host/initramfs: Fix 'nix-shell --pure --run "make run"'
Add the needed build-dependency on jq.

Signed-off-by: Demi Marie Obenour <demiobenour@gmail.com>
Message-ID: <20251105-fix-nix-shell-initramfs-v1-1-6f4632232bb4@gmail.com>
Fixes: 111b58b ("Remove unused packages")
Reviewed-by: Alyssa Ross <hi@alyssa.is>
Tested-by: Alyssa Ross <hi@alyssa.is>
Signed-off-by: Alyssa Ross <hi@alyssa.is>

host/initramfs/shell.nix
@@ -3,7 +3,7 @@
 
 import ../../lib/call-package.nix (
 { callSpectrumPackage, rootfs, pkgsStatic, stdenv
-, cryptsetup, qemu_kvm, tar2ext4, util-linux
+, cryptsetup, jq, qemu_kvm, tar2ext4, util-linux
 }:
 
 let
@@ -12,7 +12,7 @@ in
 
 initramfs.overrideAttrs ({ nativeBuildInputs ? [], env ? {}, ... }: {
   nativeBuildInputs = nativeBuildInputs ++ [
-    cryptsetup qemu_kvm tar2ext4 util-linux
+    cryptsetup jq qemu_kvm tar2ext4 util-linux
   ];
 
   env = env // {