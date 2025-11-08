24d0781d2f326b187d9ed78c25d7e158a7fa232b
Demi Marie Obenour <demiobenour@gmail.com>
2025-11-07 22:25:18 -0500

    scripts/run-qemu.sh: decrease indentation
    The rightward drift made the code harder to read.
    
    No functional change.
    
    Signed-off-by: Demi Marie Obenour <demiobenour@gmail.com>

--- scripts/run-qemu.sh
+++ scripts/run-qemu.sh
@@ -10,14 +10,14 @@ if [ -n ${ARCH+test} ]; then
 fi
 
 case $ARCH in
-	aarch64)
-		machine=virt,accel=kvm:tcg,gic-version=3,iommu=smmuv3
-		;;
-	x86_64)
-		append="console=ttyS0${append:+ $append}"
-		iommu=intel-iommu,intremap=on
-		machine=q35,accel=kvm:tcg,kernel-irqchip=split
-		;;
+aarch64)
+	machine=virt,accel=kvm:tcg,gic-version=3,iommu=smmuv3
+	;;
+x86_64)
+	append="console=ttyS0${append:+ $append}"
+	iommu=intel-iommu,intremap=on
+	machine=q35,accel=kvm:tcg,kernel-irqchip=split
+	;;
 esac
 
 i=0
@@ -26,49 +26,49 @@ while [ "$i" -lt "$#" ]; do
 	shift
 
 	case $arg in
-		-append)
-			set -- "$@" -append ${append:+"$append "}"$1"
-			i=$((i + 2))
-			shift
-			continue
-			;;
-		-device)
-			IFS=,
-			for opt in $1; do
-				case $opt in
-				*-iommu)
-					unset iommu
+	-append)
+		set -- "$@" -append ${append:+"$append "}"$1"
+		i=$((i + 2))
+		shift
+		continue
+		;;
+	-device)
+		IFS=,
+		for opt in $1; do
+			case $opt in
+			*-iommu)
+				unset iommu
+				;;
+			esac
+			break
+		done
+		unset IFS
+		;;
+	-machine)
+		set -- "$@" "$arg"
+		i=$((i + 1))
+		arg=
+
+		IFS=,
+		for opt in $1; do
+			case "$opt" in
+			virtualization=on)
+				case "$ARCH" in
+				aarch64)
+					opt="$opt,accel=tcg"
+					;;
+				*)
+					continue
 					;;
 				esac
-				break
-			done
-			unset IFS
-			;;
-		-machine)
-			set -- "$@" "$arg"
-			i=$((i + 1))
-			arg=
+				;;
+			esac
 
-			IFS=,
-			for opt in $1; do
-				case "$opt" in
-					virtualization=on)
-						case "$ARCH" in
-							aarch64)
-								opt="$opt,accel=tcg"
-								;;
-							*)
-								continue
-								;;
-						esac
-						;;
-				esac
+			arg="$arg${arg:+,}$opt"
+		done
+		unset IFS
 
-				arg="$arg${arg:+,}$opt"
-			done
-			unset IFS
-
-			shift
+		shift
 	esac
 
 	set -- "$@" "$arg"
@@ -78,12 +78,12 @@ done
 
 for arg; do
 	case "$arg" in
-		-append)
-			append=
-			;;
-		-kernel)
-			kernel=1
-			;;
+	-append)
+		append=
+		;;
+	-kernel)
+		kernel=1
+		;;
 	esac
 done
 
