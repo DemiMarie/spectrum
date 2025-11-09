DemiMarie/spectrum

commit d0a5f0abdc356f1bba676d489ba9378554fcd134

lib/nixpkgs.default.nix: update to staging-next
The addition of udevCheckHook means we need a different mechanism for
avoiding libudev.  This way, of marking systemd universally
unsupported, is probably better anyway — it should avoid us needing to
manually override every package, and let us rely on their defaults.

Signed-off-by: Alyssa Ross <hi@alyssa.is>

host/rootfs/default.nix
@@ -94,13 +94,11 @@ let
         };
       });
 
-      systemd = final.libudev-zero;
-      systemdLibs = final.libudev-zero;
-      systemdMinimal = final.libudev-zero;
-
-      seatd = super.seatd.override {
-        systemdSupport = false;
-      };
+      systemd = super.systemd.overrideAttrs ({ meta ? { }, ... }: {
+        meta = meta // {
+          platforms = [ ];
+        };
+      });
 
       tinysparql = super.tinysparql.overrideAttrs ({ mesonFlags ? [], ... }: {
         mesonFlags = mesonFlags ++ [ "-Dsystemd_user_services=false" ];
@@ -113,6 +111,8 @@ let
         # test that needs umockdev.
         withIntrospection = false;
       };
+
+      udev = final.libudev-zero;
 
       util-linux = super.util-linux.override {
         systemdSupport = false;

pkgs/cloud-hypervisor/default.nix
@@ -8,15 +8,15 @@ super.cloud-hypervisor.overrideAttrs (oldAttrs: rec {
   cargoDeps = final.rustPlatform.fetchCargoVendor {
     inherit patches;
     inherit (oldAttrs) src;
-    hash = "sha256-5+YU4ywQbBKQ/G8b6raXx/xKVYQyoJRJbgk3WpIldsc=";
+    hash = "sha256-IlwX7n12Q3iRcH93oUbF13J0d9oO5OaH4E3MsPibiGo=";
   };
 
   vhost = final.fetchFromGitHub {
     name = "vhost";
     owner = "rust-vmm";
     repo = "vhost";
-    rev = "d983ae07f78663b7d24059667376992460b571a2";
-    hash = "sha256-tSP8Ent7URu/6ehOOMP29ryLfV465ip2xrXkKu2nLYI=";
+    rev = "vhost-user-backend-v0.20.0";
+    hash = "sha256-KK1+mwYQr7YkyGT9+51v7TJael9D0lle2JXfRoTqYq8=";
   };
 
   patches = oldAttrs.patches or [] ++ [

lib/nixpkgs.default.nix
@@ -4,6 +4,6 @@
 # Generated by scripts/update-nixpkgs.sh.
 
 import (builtins.fetchTarball {
-  url = "https://github.com/NixOS/nixpkgs/archive/ec33278c3987254b9a209581e5fe8db541a1dc2d.tar.gz";
-  sha256 = "1s5xlv4gg66c9wyqz8kjnw0rwqmdg2is4kpa3h52raqci8ci6f7n";
+  url = "https://github.com/NixOS/nixpkgs/archive/6e1ffe6ba3c38d429bd15079a0935f3a86ae5e2c.tar.gz";
+  sha256 = "0mh2g0x6f8sfk9n4wl6mmwd3qwwfcdm0wjmcfqk28vb2hi6j7xxm";
 })

pkgs/cloud-hypervisor/0001-build-use-local-vhost.patch
@@ -1,4 +1,4 @@
-From 66d1565058e27c7dea06775988fa1e6f48983ee0 Mon Sep 17 00:00:00 2001
+From dec88cad3b2a6817f441c26fbe2b4fef46759d17 Mon Sep 17 00:00:00 2001
 From: Alyssa Ross <alyssa.ross@unikie.com>
 Date: Wed, 28 Sep 2022 12:18:19 +0000
 Subject: [PATCH 1/2] build: use local vhost
@@ -9,44 +9,46 @@ SPDX-License-Identifier: Apache-2.0 AND LicenseRef-BSD-3-Clause-Google
 Signed-off-by: Alyssa Ross <alyssa.ross@unikie.com>
 Signed-off-by: Alyssa Ross <hi@alyssa.is>
 ---
- Cargo.lock | 2 --
+ Cargo.lock | 4 ----
  Cargo.toml | 4 ++--
- 2 files changed, 2 insertions(+), 4 deletions(-)
+ 2 files changed, 2 insertions(+), 6 deletions(-)
 
 diff --git a/Cargo.lock b/Cargo.lock
-index 1af12b40f..9cf07b994 100644
+index 305e31cee..9724641d4 100644
 --- a/Cargo.lock
 +++ b/Cargo.lock
-@@ -2309,7 +2309,6 @@ dependencies = [
+@@ -2276,8 +2276,6 @@ dependencies = [
  [[package]]
  name = "vhost"
- version = "0.12.1"
--source = "git+https://github.com/rust-vmm/vhost?rev=d983ae0#d983ae07f78663b7d24059667376992460b571a2"
+ version = "0.14.0"
+-source = "registry+https://github.com/rust-lang/crates.io-index"
+-checksum = "2a4dcad85a129d97d5d4b2f3c47a4affdeedd76bdcd02094bcb5d9b76cac2d05"
  dependencies = [
   "bitflags 2.9.0",
   "libc",
-@@ -2321,7 +2320,6 @@ dependencies = [
+@@ -2289,8 +2287,6 @@ dependencies = [
  [[package]]
  name = "vhost-user-backend"
- version = "0.16.1"
--source = "git+https://github.com/rust-vmm/vhost?rev=d983ae0#d983ae07f78663b7d24059667376992460b571a2"
+ version = "0.20.0"
+-source = "registry+https://github.com/rust-lang/crates.io-index"
+-checksum = "e183205a9ba7cb9c47fcb0fc0a07fc295a110efbb11ab78ad0d793b0a38a7bde"
  dependencies = [
   "libc",
   "log",
 diff --git a/Cargo.toml b/Cargo.toml
-index 777e6356f..f3f6d9c3f 100644
+index f05de398e..380a06767 100644
 --- a/Cargo.toml
 +++ b/Cargo.toml
 @@ -113,8 +113,8 @@ seccompiler = "0.5.0"
- vfio-bindings = { git = "https://github.com/rust-vmm/vfio", branch = "main" }
- vfio-ioctls = { git = "https://github.com/rust-vmm/vfio", branch = "main", default-features = false }
- vfio_user = { git = "https://github.com/rust-vmm/vfio-user", branch = "main" }
--vhost = { git = "https://github.com/rust-vmm/vhost", rev = "d983ae0" }
--vhost-user-backend = { git = "https://github.com/rust-vmm/vhost", rev = "d983ae0" }
+ vfio-bindings = { version = "0.5.0", default-features = false }
+ vfio-ioctls = { version = "0.5.0", default-features = false }
+ vfio_user = { version = "0.1.0", default-features = false }
+-vhost = { version = "0.14.0", default-features = false }
+-vhost-user-backend = { version = "0.20.0", default-features = false }
 +vhost = { path = "../vhost/vhost" }
 +vhost-user-backend = { path = "../vhost/vhost-user-backend" }
- virtio-bindings = "0.2.4"
- virtio-queue = "0.14.0"
+ virtio-bindings = "0.2.6"
+ virtio-queue = "0.16.0"
  vm-fdt = { git = "https://github.com/rust-vmm/vm-fdt", branch = "main" }
 -- 
 2.50.0

pkgs/cloud-hypervisor/vhost/0001-vhost_user-add-get_size-to-MsgHeader.patch
@@ -1,4 +1,4 @@
-From 7e4a19ef7d52db929f397d4c799d813cc4fdd1e4 Mon Sep 17 00:00:00 2001
+From 667b6aa7d3fd81a832d57b54e24bdef1614ebdd5 Mon Sep 17 00:00:00 2001
 From: Alyssa Ross <hi@alyssa.is>
 Date: Thu, 24 Oct 2024 17:11:17 +0200
 Subject: [PATCH 1/4] vhost_user: add get_size() to MsgHeader
@@ -14,7 +14,7 @@ Signed-off-by: Alyssa Ross <hi@alyssa.is>
  2 files changed, 11 insertions(+), 10 deletions(-)
 
 diff --git a/vhost/src/vhost_user/gpu_message.rs b/vhost/src/vhost_user/gpu_message.rs
-index 7aa1726..1ee25f4 100644
+index 6be26f20d..75073dc4c 100644
 --- a/vhost/src/vhost_user/gpu_message.rs
 +++ b/vhost/src/vhost_user/gpu_message.rs
 @@ -135,11 +135,6 @@ impl<R: Req> VhostUserGpuMsgHeader<R> {
@@ -41,7 +41,7 @@ index 7aa1726..1ee25f4 100644
  
  // Bit mask for vhost-user-gpu protocol feature flags.
 diff --git a/vhost/src/vhost_user/message.rs b/vhost/src/vhost_user/message.rs
-index c66bd44..8359eb1 100644
+index c66bd4468..8359eb1fd 100644
 --- a/vhost/src/vhost_user/message.rs
 +++ b/vhost/src/vhost_user/message.rs
 @@ -70,6 +70,9 @@ pub(super) trait MsgHeader: ByteValued + Copy + Default + VhostUserMsgValidator
@@ -78,5 +78,5 @@ index c66bd44..8359eb1 100644
      pub fn set_size(&mut self, size: u32) {
          self.size = size;
 -- 
-2.47.0
+2.50.0
 

pkgs/cloud-hypervisor/vhost/0002-vhost-fix-receiving-reply-payloads.patch
@@ -1,10 +1,11 @@
-From b89f211b3a002aebce9bc8d9dbd159b9c88fd3b3 Mon Sep 17 00:00:00 2001
+From d56efa072f28c203bdf6b0c6e62a04984ebb1e28 Mon Sep 17 00:00:00 2001
 From: David Stevens <stevensd@chromium.org>
 Date: Wed, 15 Jun 2022 15:56:18 +0900
 Subject: [PATCH 2/4] vhost: fix receiving reply payloads
 SPDX-FileCopyrightText: 2019 Alibaba Cloud Computing. All rights reserved.
 SPDX-FileCopyrightText: The Chromium OS Authors
 SPDX-FileCopyrightText: 2023 Alyssa Ross <hi@alyssa.is>
+SPDX-FileCopyrightText: 2024 Red Hat, Inc.
 SPDX-License-Identifier: Apache-2.0
 
 The existing code confuses the length of the request with the length of
@@ -17,12 +18,13 @@ determining payload size after reading the reply header.
 Co-authored-by: Alyssa Ross <hi@alyssa.is>
 Signed-off-by: Alyssa Ross <hi@alyssa.is>
 ---
- vhost/src/vhost_user/connection.rs | 29 ++++++++++++-----------------
- vhost/src/vhost_user/frontend.rs   | 16 +++-------------
- 2 files changed, 15 insertions(+), 30 deletions(-)
+ vhost/src/vhost_user/connection.rs      | 29 ++++++++++---------------
+ vhost/src/vhost_user/frontend.rs        | 16 +++-----------
+ vhost/src/vhost_user/gpu_backend_req.rs | 14 +++++-------
+ 3 files changed, 21 insertions(+), 38 deletions(-)
 
 diff --git a/vhost/src/vhost_user/connection.rs b/vhost/src/vhost_user/connection.rs
-index 4fca9c5..0ce7215 100644
+index 4fca9c5bc..0ce72154e 100644
 --- a/vhost/src/vhost_user/connection.rs
 +++ b/vhost/src/vhost_user/connection.rs
 @@ -543,7 +543,7 @@ impl<H: MsgHeader> Endpoint<H> {
@@ -82,10 +84,10 @@ index 4fca9c5..0ce7215 100644
  }
  
 diff --git a/vhost/src/vhost_user/frontend.rs b/vhost/src/vhost_user/frontend.rs
-index 0fb548b..9e26474 100644
+index ea6284980..195a6af1e 100644
 --- a/vhost/src/vhost_user/frontend.rs
 +++ b/vhost/src/vhost_user/frontend.rs
-@@ -738,23 +738,13 @@ impl FrontendInternal {
+@@ -756,23 +756,13 @@ impl FrontendInternal {
          &mut self,
          hdr: &VhostUserMsgHeader<FrontendReq>,
      ) -> VhostUserResult<(T, Vec<u8>, Option<Vec<File>>)> {
@@ -112,6 +114,52 @@ index 0fb548b..9e26474 100644
              return Err(VhostUserError::InvalidMessage);
          }
  
+diff --git a/vhost/src/vhost_user/gpu_backend_req.rs b/vhost/src/vhost_user/gpu_backend_req.rs
+index 140063093..f7160c9c4 100644
+--- a/vhost/src/vhost_user/gpu_backend_req.rs
++++ b/vhost/src/vhost_user/gpu_backend_req.rs
+@@ -437,9 +437,8 @@ mod tests {
+             let _: () = backend.update_scanout(&request, &payload).unwrap();
+         });
+ 
+-        let mut recv_buf = [0u8; 4096];
+-        let (hdr, req_body, recv_buf_len, fds) = frontend
+-            .recv_payload_into_buf::<VhostUserGpuUpdate>(&mut recv_buf)
++        let (hdr, req_body, recv_buf, fds) = frontend
++            .recv_payload_into_buf::<VhostUserGpuUpdate>()
+             .unwrap();
+         assert!(fds.is_none());
+         assert_hdr(
+@@ -449,7 +448,7 @@ mod tests {
+         );
+         assert_eq!(req_body, request);
+ 
+-        assert_eq!(&payload[..], &recv_buf[..recv_buf_len]);
++        assert_eq!(&payload[..], recv_buf);
+ 
+         sender_thread.join().expect("Failed to send!");
+     }
+@@ -611,9 +610,8 @@ mod tests {
+             let _: () = backend.cursor_update(&request, &payload).unwrap();
+         });
+ 
+-        let mut recv_buf = vec![0u8; 1 + size_of_val(&payload)];
+-        let (hdr, req_body, recv_buf_len, fds) = frontend
+-            .recv_payload_into_buf::<VhostUserGpuCursorUpdate>(&mut recv_buf)
++        let (hdr, req_body, recv_buf, fds) = frontend
++            .recv_payload_into_buf::<VhostUserGpuCursorUpdate>()
+             .unwrap();
+         assert!(fds.is_none());
+         assert_hdr(
+@@ -623,7 +621,7 @@ mod tests {
+         );
+         assert_eq!(req_body, request);
+ 
+-        assert_eq!(&payload[..], &recv_buf[..recv_buf_len]);
++        assert_eq!(&payload[..], recv_buf);
+ 
+         sender_thread.join().expect("Failed to send!");
+     }
 -- 
-2.47.0
+2.50.0
 

pkgs/cloud-hypervisor/vhost/0003-vhost_user-add-shared-memory-region-support.patch
@@ -1,4 +1,4 @@
-From 941c789933574a9491ac0c6d0d8063f474090eee Mon Sep 17 00:00:00 2001
+From 7012cacaee3c8c8ce1bd6992be6ae16879110d23 Mon Sep 17 00:00:00 2001
 From: David Stevens <stevensd@chromium.org>
 Date: Wed, 15 Jun 2022 16:45:12 +0900
 Subject: [PATCH 3/4] vhost_user: add shared memory region support
@@ -31,34 +31,35 @@ Signed-off-by: Alyssa Ross <alyssa.ross@unikie.com>
 [renumbered for crosvm commit e428c4ba88a26695f63929c680b4101a9b8d5cbc]
 Signed-off-by: Alyssa Ross <hi@alyssa.is>
 ---
- vhost-user-backend/src/handler.rs            |  11 +-
+ vhost-user-backend/src/handler.rs            |  12 +-
  vhost/src/vhost_user/backend_req.rs          |  20 ++-
  vhost/src/vhost_user/backend_req_handler.rs  |  15 +++
  vhost/src/vhost_user/dummy_backend.rs        |   4 +
  vhost/src/vhost_user/frontend.rs             |  24 ++++
  vhost/src/vhost_user/frontend_req_handler.rs |  53 +++++++-
  vhost/src/vhost_user/message.rs              | 126 +++++++++++++++++++
- 7 files changed, 243 insertions(+), 10 deletions(-)
+ 7 files changed, 243 insertions(+), 11 deletions(-)
 
 diff --git a/vhost-user-backend/src/handler.rs b/vhost-user-backend/src/handler.rs
-index 9217870..0bec765 100644
+index 27e8e9a1f..b242a72c9 100644
 --- a/vhost-user-backend/src/handler.rs
 +++ b/vhost-user-backend/src/handler.rs
-@@ -17,9 +17,10 @@ use crate::bitmap::{BitmapReplace, MemRegionBitmap, MmapLogReg};
+@@ -17,10 +17,10 @@ use crate::bitmap::{BitmapReplace, MemRegionBitmap, MmapLogReg};
  #[cfg(feature = "postcopy")]
  use userfaultfd::{Uffd, UffdBuilder};
  use vhost::vhost_user::message::{
 -    VhostTransferStateDirection, VhostTransferStatePhase, VhostUserConfigFlags, VhostUserLog,
--    VhostUserMemoryRegion, VhostUserProtocolFeatures, VhostUserSingleMemoryRegion,
--    VhostUserVirtioFeatures, VhostUserVringAddrFlags, VhostUserVringState,
+-    VhostUserMemoryRegion, VhostUserProtocolFeatures, VhostUserSharedMsg,
+-    VhostUserSingleMemoryRegion, VhostUserVirtioFeatures, VhostUserVringAddrFlags,
+-    VhostUserVringState,
 +    VhostSharedMemoryRegion, VhostTransferStateDirection, VhostTransferStatePhase,
 +    VhostUserConfigFlags, VhostUserLog, VhostUserMemoryRegion, VhostUserProtocolFeatures,
-+    VhostUserSingleMemoryRegion, VhostUserVirtioFeatures, VhostUserVringAddrFlags,
-+    VhostUserVringState,
++    VhostUserSharedMsg, VhostUserSingleMemoryRegion, VhostUserVirtioFeatures,
++    VhostUserVringAddrFlags, VhostUserVringState,
  };
  use vhost::vhost_user::GpuBackend;
  use vhost::vhost_user::{
-@@ -663,6 +664,10 @@ where
+@@ -671,6 +671,10 @@ where
          Ok(())
      }
  
@@ -70,7 +71,7 @@ index 9217870..0bec765 100644
          &mut self,
          direction: VhostTransferStateDirection,
 diff --git a/vhost/src/vhost_user/backend_req.rs b/vhost/src/vhost_user/backend_req.rs
-index f95f812..e6d677c 100644
+index eb5ef65af..69665ce0e 100644
 --- a/vhost/src/vhost_user/backend_req.rs
 +++ b/vhost/src/vhost_user/backend_req.rs
 @@ -49,12 +49,16 @@ impl BackendInternal {
@@ -111,10 +112,10 @@ index f95f812..e6d677c 100644
  
  #[cfg(test)]
 diff --git a/vhost/src/vhost_user/backend_req_handler.rs b/vhost/src/vhost_user/backend_req_handler.rs
-index 635766d..a8eaadf 100644
+index d74b04558..076b09362 100644
 --- a/vhost/src/vhost_user/backend_req_handler.rs
 +++ b/vhost/src/vhost_user/backend_req_handler.rs
-@@ -73,6 +73,7 @@ pub trait VhostUserBackendReqHandler {
+@@ -74,6 +74,7 @@ pub trait VhostUserBackendReqHandler {
      fn get_max_mem_slots(&self) -> Result<u64>;
      fn add_mem_region(&self, region: &VhostUserSingleMemoryRegion, fd: File) -> Result<()>;
      fn remove_mem_region(&self, region: &VhostUserSingleMemoryRegion) -> Result<()>;
@@ -122,7 +123,7 @@ index 635766d..a8eaadf 100644
      fn set_device_state_fd(
          &self,
          direction: VhostTransferStateDirection,
-@@ -137,6 +138,7 @@ pub trait VhostUserBackendReqHandlerMut {
+@@ -139,6 +140,7 @@ pub trait VhostUserBackendReqHandlerMut {
      fn get_max_mem_slots(&mut self) -> Result<u64>;
      fn add_mem_region(&mut self, region: &VhostUserSingleMemoryRegion, fd: File) -> Result<()>;
      fn remove_mem_region(&mut self, region: &VhostUserSingleMemoryRegion) -> Result<()>;
@@ -130,7 +131,7 @@ index 635766d..a8eaadf 100644
      fn set_device_state_fd(
          &mut self,
          direction: VhostTransferStateDirection,
-@@ -268,6 +270,10 @@ impl<T: VhostUserBackendReqHandlerMut> VhostUserBackendReqHandler for Mutex<T> {
+@@ -274,6 +276,10 @@ impl<T: VhostUserBackendReqHandlerMut> VhostUserBackendReqHandler for Mutex<T> {
          self.lock().unwrap().remove_mem_region(region)
      }
  
@@ -141,7 +142,7 @@ index 635766d..a8eaadf 100644
      fn set_device_state_fd(
          &self,
          direction: VhostTransferStateDirection,
-@@ -609,6 +615,15 @@ impl<S: VhostUserBackendReqHandler> BackendReqHandler<S> {
+@@ -634,6 +640,15 @@ impl<S: VhostUserBackendReqHandler> BackendReqHandler<S> {
                  let res = self.backend.remove_mem_region(&msg);
                  self.send_ack_message(&hdr, res)?;
              }
@@ -158,10 +159,10 @@ index 635766d..a8eaadf 100644
                  let file = take_single_file(files).ok_or(Error::IncorrectFds)?;
                  let msg =
 diff --git a/vhost/src/vhost_user/dummy_backend.rs b/vhost/src/vhost_user/dummy_backend.rs
-index ac36cdc..f0b761c 100644
+index a45d3b47f..9b2040d6f 100644
 --- a/vhost/src/vhost_user/dummy_backend.rs
 +++ b/vhost/src/vhost_user/dummy_backend.rs
-@@ -302,6 +302,10 @@ impl VhostUserBackendReqHandlerMut for DummyBackendReqHandler {
+@@ -310,6 +310,10 @@ impl VhostUserBackendReqHandlerMut for DummyBackendReqHandler {
          Ok(())
      }
  
@@ -173,10 +174,10 @@ index ac36cdc..f0b761c 100644
          &mut self,
          _direction: VhostTransferStateDirection,
 diff --git a/vhost/src/vhost_user/frontend.rs b/vhost/src/vhost_user/frontend.rs
-index 9e26474..86fe1d4 100644
+index 195a6af1e..41b49cbd3 100644
 --- a/vhost/src/vhost_user/frontend.rs
 +++ b/vhost/src/vhost_user/frontend.rs
-@@ -76,6 +76,9 @@ pub trait VhostUserFrontend: VhostBackend {
+@@ -79,6 +79,9 @@ pub trait VhostUserFrontend: VhostBackend {
      /// Remove a guest memory mapping from vhost.
      fn remove_mem_region(&mut self, region: &VhostUserMemoryRegionInfo) -> Result<()>;
  
@@ -186,7 +187,7 @@ index 9e26474..86fe1d4 100644
      /// Sends VHOST_USER_POSTCOPY_ADVISE msg to the backend
      /// initiating the beginning of the postcopy process.
      /// Backend will return a userfaultfd.
-@@ -549,6 +552,27 @@ impl VhostUserFrontend for Frontend {
+@@ -567,6 +570,27 @@ impl VhostUserFrontend for Frontend {
          node.wait_for_ack(&hdr).map_err(|e| e.into())
      }
  
@@ -215,7 +216,7 @@ index 9e26474..86fe1d4 100644
      fn postcopy_advise(&mut self) -> Result<File> {
          let mut node = self.node();
 diff --git a/vhost/src/vhost_user/frontend_req_handler.rs b/vhost/src/vhost_user/frontend_req_handler.rs
-index 77d4bf5..f26cd64 100644
+index 77d4bf55a..f26cd6491 100644
 --- a/vhost/src/vhost_user/frontend_req_handler.rs
 +++ b/vhost/src/vhost_user/frontend_req_handler.rs
 @@ -52,6 +52,16 @@ pub trait VhostUserFrontendReqHandler {
@@ -321,7 +322,7 @@ index 77d4bf5..f26cd64 100644
              let def_err = libc::EINVAL;
              let val = match res {
 diff --git a/vhost/src/vhost_user/message.rs b/vhost/src/vhost_user/message.rs
-index 8359eb1..f090466 100644
+index 8359eb1fd..b847885a4 100644
 --- a/vhost/src/vhost_user/message.rs
 +++ b/vhost/src/vhost_user/message.rs
 @@ -172,6 +172,8 @@ enum_value! {
@@ -479,5 +480,5 @@ index 8359eb1..f090466 100644
  mod tests {
      use super::*;
 -- 
-2.47.0
+2.50.0
 

commit 36d2f0bd5caf297efec0e41f42e5983ae9dc1cb1

host/rootfs: fix file chooser portal
Since libfuse commit bb9cecb ("Use posix_spawn instead of fork+exec"),
failure to execute fusermount is no longer ignored, so we have to make
sure fusermount is in PATH.

Fixes: d0a5f0a ("lib/nixpkgs.default.nix: update to staging-next")
Signed-off-by: Alyssa Ross <hi@alyssa.is>

host/rootfs/default.nix
@@ -60,7 +60,7 @@ let
     })
 
   # Take kmod from pkgsGui since we use pkgsGui.kmod.lib below anyway.
-  ] ++ (with pkgsGui; [ cosmic-files crosvm foot kmod systemd ]);
+  ] ++ (with pkgsGui; [ cosmic-files crosvm foot fuse3 kmod systemd ]);
 
   nixosAllHardware = nixos ({ modulesPath, ... }: {
     imports = [ (modulesPath + "/profiles/all-hardware.nix") ];