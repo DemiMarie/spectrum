11edc61629134b7359c86935648494162af835ba
Alyssa Ross <hi@alyssa.is>
2025-11-26 22:33:55 +0100

    pkgs: s6: backport s6-softlimit -P
    We'll need this to run PipeWire as non-root.
    
    Signed-off-by: Alyssa Ross <hi@alyssa.is>
    Message-ID: <20251126213407.1773744-2-hi@alyssa.is>

--- pkgs/skaware-packages/default.nix
+++ pkgs/skaware-packages/default.nix
@@ -4,6 +4,15 @@
 import ../../lib/overlay-package.nix [ "skawarePackages" ] ({ final, super }:
 
 super.skawarePackages.overrideScope (_: prev: {
+  s6 = prev.s6.overrideAttrs ({ patches ? [], ... }: {
+    patches = patches ++ [
+      (final.fetchpatch {
+        url = "https://git.skarnet.org/cgi-bin/cgit.cgi/s6/patch/?id=c3a8ef7034fb2bc02f35381a8970ac026822a810";
+        hash = "sha256-lgCoPbEYru6/a2bpVpLsZ2Rq2OHhNVs0lDgFO/df1Aw=";
+      })
+    ];
+  });
+
   mdevd = prev.mdevd.overrideAttrs ({ patches ? [], ... }: {
     patches = patches ++ [
       (final.fetchpatch {
