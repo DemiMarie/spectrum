49f33a139249b6270d3a9491fc0ba343c637ab42
Demi Marie Obenour <demiobenour@gmail.com>
2025-11-29 04:50:02 -0500

    release: Create directory with system update
    Whenever a release is made, create a directory with the release files to
    be used for an update.  After its SHA256SSUMS file is signed, the file
    is ready to be uploaded to a server for users to update from.
    
    Signed-off-by: Demi Marie Obenour <demiobenour@gmail.com>
    Reviewed-by: Alyssa Ross <hi@alyssa.is>
    Message-ID: <20251129-updates-v6-5-9edb87a2e509@gmail.com>
    Signed-off-by: Alyssa Ross <hi@alyssa.is>

---
+++ release/update.nix
@@ -0,0 +1,33 @@
+# SPDX-License-Identifier: MIT
+# SPDX-FileCopyrightText: 2021-2024 Alyssa Ross <hi@alyssa.is>
+# SPDX-FileCopyrightText: 2025 Demi Marie Obenour <demiobenour@gmail.com>
+
+import ../lib/call-package.nix (
+{ callSpectrumPackage, config, runCommand, stdenv }:
+
+let
+  efi = import ../host/efi.nix {};
+in
+runCommand "spectrum-update-directory" {
+  __structuredAttrs = true;
+  unsafeDiscardReferences = { out = true; };
+  dontFixup = true;
+  env = { VERSION = config.version; };
+} ''
+  # stdenv sets -eo pipefail, but not -u
+  set -u
+  mkdir -- "$out"
+  cd -- "$out"
+  read -r roothash < ${efi.rootfs}/rootfs.verity.roothash
+  if ! [[ "$roothash" =~ ^[0-9a-f]{64}$ ]]; then
+    printf 'Internal error: bad root hash %q\n' "$roothash"
+    exit 1
+  fi
+  cp -- ${efi} "Spectrum_$VERSION.efi"
+  cp -- ${efi.rootfs}/rootfs.verity.superblock "Spectrum_''${VERSION}_''${roothash:32:32}.verity"
+  cp -- ${efi.rootfs}/rootfs "Spectrum_''${VERSION}_''${roothash:0:32}.root"
+  sha256sum -b "Spectrum_$VERSION.efi" \
+    "Spectrum_''${VERSION}_''${roothash:32:32}.verity" \
+    "Spectrum_''${VERSION}_''${roothash:0:32}.root" > SHA256SUMS
+  ''
+) (_: {})
