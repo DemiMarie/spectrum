DemiMarie/spectrum

commit 7a7c08084cb6a7199da1808afb58c7fec6b8c7f9

release: Compress installation images and remove live image
systemd-sysupdate will fail if the OS image does not fit in the
partitions that the installer created.  Therefor, the partitions need to
be very large so that there is plenty of room for the OS to grow.
Furthermore, systemd-sysupdate requires both A and B copies of both the
root and verity partitions.

mkfs.ext4 is not able to produce images with files large enough to hold
both the primary and backup copy of the root partition [1].  Reducing
the sizes of partitions to be little greater than the size of the root
filesystem image does not help.  The produced file is still too large.
Therefore, compress the image, which causes it to be small enough that
mkfs.ext4 can handle it.

This breaks the option to use the installer as a live image.  Therefore,
remove it.  This option will return once Spectrum switches to the GNOME
OS installer [2].  However, it is still possible to build a live image
that is separate from the installer.  Document how to build and use it.

GRUB2 does support compressed loopback images, but these presumably
buffer the whole image in memory.  Since the entire installer will be
replaced, making it work is not considered worthwhile.

[1]: https://github.com/tytso/e2fsprogs/issues/254
[2]: https://spectrum-os.org/lists/archives/spectrum-devel/87wm4dlkhz.fsf@alyssa.is/

Signed-off-by: Demi Marie Obenour <demiobenour@gmail.com>
Reviewed-by: Alyssa Ross <hi@alyssa.is>
Message-ID: <20251129-updates-v6-2-9edb87a2e509@gmail.com>
Signed-off-by: Alyssa Ross <hi@alyssa.is>

release/combined/eosimages.nix
@@ -14,9 +14,9 @@ runCommand "eosimages.img" {
 } ''
   mkdir dir
   cd dir
-  ln -s $image $imageName
-  sha256sum $imageName > $imageName.sha256
-  tar -chf $NIX_BUILD_TOP/eosimages.tar *
-  tar2ext4 -i $NIX_BUILD_TOP/eosimages.tar -o $out
+  ln -s -- $image $imageName
+  gzip -1 < $image > $imageName.gz
+  sha256sum -- $imageName.gz > $imageName.gz.sha256
+  tar -ch -- $imageName.gz $imageName.gz.sha256 | tar2ext4 -o $out
   e2label $out eosimages
 '') (_: {})

host/initramfs/Makefile
@@ -41,14 +41,6 @@ build/live.img: ../../scripts/format-uuid.sh ../../scripts/make-gpt.sh ../../scr
 	    $(ROOT_FS_IMAGE):root:$$(../../scripts/format-uuid.sh "$$(head -c 32 $(ROOT_FS_VERITY_ROOTHASH))")
 	mv $@.tmp $@
 
-build/loop.tar: build/live.img
-	$(TAR) -cf $@ build/live.img
-
-build/loop.img: ../../scripts/make-gpt.sh ../../scripts/sfdisk-field.awk build/loop.ext4
-	../../scripts/make-gpt.sh $@.tmp \
-	    build/loop.ext4:56a3bbc3-aefa-43d9-a64d-7b3fd59bbc4e
-	mv $@.tmp $@
-
 clean:
 	rm -rf build
 .PHONY: clean

host/initramfs/etc/probe
@@ -2,26 +2,6 @@
 # SPDX-License-Identifier: EUPL-1.2+
 # SPDX-FileCopyrightText: 2021 Alyssa Ross <hi@alyssa.is>
 
-if -n {
-  # If this is a Spectrum installer eosimages partition, we might be
-  # booting from the installer, and should loopback mount the images.
-  importas -i mdev MDEV
-  if {
-    backtick -E type { lsblk -lnpo PARTTYPE $mdev }
-    test $type = 56a3bbc3-aefa-43d9-a64d-7b3fd59bbc4e
-  }
-  if {
-    forx -pE module { ext4 loop }
-    modprobe $module
-  }
-  backtick -E uuid { lsblk -lnpo PARTUUID $mdev }
-  if { mkdir -p /mnt/${uuid} }
-  if { mount $mdev /mnt/${uuid} }
-  find /mnt/${uuid} -name *.img -exec
-  losetup -Pf {}
-  ;
-}
-
 # Check whether we now have all the partitions we need to boot.
 
 importas -i rootfs_uuid ROOTFS_UUID

release/checks/integration/default.nix
@@ -7,8 +7,7 @@ import ../../../lib/call-package.nix (
 }:
 
 let
-  combined = callSpectrumPackage ../../combined {};
-  live = combined.eosimages.image;
+  live = callSpectrumPackage ../../live {};
 
   appimage = writeShellScript "test.appimage" ''
     #!/bin/execlineb -P
@@ -87,7 +86,6 @@ stdenv.mkDerivation (finalAttrs: {
   env = {
     QEMU_SYSTEM = "qemu-system-${stdenv.hostPlatform.qemuArch} -nographic";
     EFI_PATH = "${qemu_kvm}/share/qemu/edk2-${stdenv.hostPlatform.qemuArch}-code.fd";
-    COMBINED_PATH = combined;
     LIVE_PATH = live;
     USER_DATA_PATH = userData;
   };

release/checks/integration/meson.build
@@ -11,7 +11,7 @@ run_qemu = find_program('../../../scripts/run-qemu.sh')
 
 lib = static_library('spectrum-integration-test', 'lib.c')
 
-foreach test : ['appimage', 'late-serial', 'networking', 'portal', 'try']
+foreach test : ['appimage', 'late-serial', 'networking', 'portal']
   test(test, executable(test, test + '.c', link_with : lib),
     timeout : 400,
     args : [run_qemu])

release/checks/integration/try.c
@@ -1,29 +1,0 @@
-// SPDX-License-Identifier: EUPL-1.2+
-// SPDX-FileCopyrightText: 2025 Alyssa Ross <hi@alyssa.is>
-
-#include "lib.h"
-
-#include <stdlib.h>
-#include <string.h>
-
-void test(struct config c)
-{
-	struct vm *vm;
-
-	c.drives.img = getenv_or_die("COMBINED_PATH");
-
-	vm = start_qemu(c);
-
-	start_console_thread(vm, "GNU GRUB ");
-	wait_for_prompt(vm);
-
-	start_console_thread(vm, "~ # ");
-
-	// Assume that Try Spectrum is the first menu entry.
-	if (fputc('\n', vm_console_writer(vm)) == EOF) {
-		fputs("error writing to console\n", stderr);
-		exit(EXIT_FAILURE);
-	}
-
-	wait_for_prompt(vm);
-}

release/combined/grub.cfg.in
@@ -15,11 +15,6 @@ set gfxpayload=keep
 terminal_output gfxterm
 terminal_output console
 
-menuentry "Try Spectrum" {
-  loopback live (hd0,gpt3)/Spectrum-0.0-x86_64-generic.0.Live.img
-  chainloader (live,gpt1)/EFI/Linux/spectrum.efi
-}
-
 menuentry "Install Spectrum" {
   set root=(hd0,gpt2)
   linux @linux@ @kernelParams@

commit de3a8808f390bdce421077a62107f1d8bdeff22c

release/combined: remove obsolete ln
Fixes: 7a7c080 ("release: Compress installation images and remove live image")
Signed-off-by: Alyssa Ross <hi@alyssa.is>

release/combined/eosimages.nix
@@ -14,7 +14,6 @@ runCommand "eosimages.img" {
 } ''
   mkdir dir
   cd dir
-  ln -s -- $image $imageName
   gzip -1 < $image > $imageName.gz
   sha256sum -- $imageName.gz > $imageName.gz.sha256
   tar -ch -- $imageName.gz $imageName.gz.sha256 | tar2ext4 -o $out