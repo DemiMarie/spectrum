DemiMarie/spectrum

commit ced08f7ebdadfb07d34ef6a0e6016e8e6d2a24b1

host/rootfs: Switch to systemd-udevd
This replaces the mdevd uevent daemon with systemd-udevd, which supports
much more hardware and is therefore to be preferred for a full desktop
environment.  Various Wayland compositors integrate with systemd-udevd,
allowing them to discover devices as they appear rather than having to
only use devices plugged in when the compositor is started.
Additionally, systemd-udevd has quirks for various devices that are
needed to support the wide variety of hardware end-users have.

New dependencies are added to ensure that udev coldplug has finished
before any non-trivial services are started.
systemd-udev-trigger.service runs 'udevadm trigger' and has
Before=sysinit.target, so anything that is not an early boot service
can assume 'udevadm trigger' has run.

systemd-udevd unsets PATH before it runs programs.  Therefore, manually
set it to /usr/bin before invoking any scripts.

systemd-udevd provides the 'udevadm wait' command to wait for a device
to appear and be processed.  This replaces all uses of /etc/mdev/wait,
so this script is removed.  /etc/mdev/listen and /run/wait only existed
to wake up /etc/mdev/wait, so they are also removed.

The command-line options to 'udevadm trigger' are taken from
systemd-udev-trigger.service on my Arch machine.  Using the wrong
arguments leads to problems that take a very long time to debug.

Signed-off-by: Demi Marie Obenour <demiobenour@gmail.com>
Message-ID: <20251107-udev-v6-2-176246281424@gmail.com>
Co-authored-by: Alyssa Ross <hi@alyssa.is>
Signed-off-by: Alyssa Ross <hi@alyssa.is>

host/rootfs/default.nix
@@ -11,7 +11,7 @@ pkgsStatic.callPackage (
 { spectrum-host-tools
 , lib, stdenvNoCC, nixos, runCommand, writeClosure, erofs-utils, s6-rc
 , busybox, cloud-hypervisor, cryptsetup, dbus, execline, inkscape
-, iproute2, inotify-tools, jq, kmod, mdevd, s6, s6-linux-init, socat
+, iproute2, inotify-tools, jq, mdevd, s6, s6-linux-init, socat
 , util-linuxMinimal, virtiofsd, xorg, xdg-desktop-portal-spectrum-host
 }:
 
@@ -22,59 +22,11 @@ let
     mapAttrsToList systems trivial;
 
   pkgsGui = pkgsMusl.extend (
-    final: super:
+    _: super:
     (optionalAttrs (systems.equals pkgsMusl.stdenv.hostPlatform super.stdenv.hostPlatform) {
       flatpak = super.flatpak.override {
         withMalcontent = false;
       };
-
-      libgudev = super.libgudev.overrideAttrs ({ ... }: {
-        # Tests use umockdev, which is not compatible with libudev-zero.
-        doCheck = false;
-      });
-
-      qt6 = super.qt6.overrideScope (_: prev: {
-        qttranslations = prev.qttranslations.override {
-          qttools = prev.qttools.override {
-            qtbase = prev.qtbase.override {
-              qttranslations = null;
-              systemdSupport = false;
-            };
-            qtdeclarative = null;
-          };
-        };
-
-        qtbase = prev.qtbase.override {
-          systemdSupport = false;
-        };
-      });
-
-      systemd = super.systemd.overrideAttrs ({ meta ? { }, ... }: {
-        meta = meta // {
-          platforms = [ ];
-        };
-      });
-
-      upower = super.upower.override {
-        # Not ideal, but it's the best way to get rid of an installed
-        # test that needs umockdev.
-        withIntrospection = false;
-      };
-
-      udev = final.libudev-zero;
-
-      weston = super.weston.overrideAttrs ({ mesonFlags ? [], ... }: {
-        mesonFlags = mesonFlags ++ [
-          "-Dsystemd=false"
-        ];
-      });
-
-      xdg-desktop-portal = (super.xdg-desktop-portal.override {
-        enableSystemd = false;
-      }).overrideAttrs ({ ... }: {
-        # Tests use umockdev.
-        doCheck = false;
-      });
     })
   );
 
@@ -82,7 +34,7 @@ let
 
   packages = [
     cloud-hypervisor cryptsetup dbus execline inotify-tools iproute2
-    jq kmod mdevd s6 s6-linux-init s6-rc socat spectrum-host-tools
+    jq mdevd s6 s6-linux-init s6-rc socat spectrum-host-tools
     virtiofsd xdg-desktop-portal-spectrum-host
 
     (busybox.override {
@@ -90,6 +42,7 @@ let
         CONFIG_CHATTR n
         CONFIG_DEPMOD n
         CONFIG_FINDFS n
+        CONFIG_HALT n
         CONFIG_INIT n
         CONFIG_INSMOD n
         CONFIG_IP n
@@ -100,10 +53,14 @@ let
         CONFIG_MODINFO n
         CONFIG_MODPROBE n
         CONFIG_MOUNT n
+        CONFIG_POWEROFF n
+        CONFIG_REBOOT n
         CONFIG_RMMOD n
       '';
     })
-  ] ++ (with pkgsGui; [ cosmic-files crosvm foot ]);
+
+  # Take kmod from pkgsGui since we use pkgsGui.kmod.lib below anyway.
+  ] ++ (with pkgsGui; [ cosmic-files crosvm foot kmod systemd ]);
 
   nixosAllHardware = nixos ({ modulesPath, ... }: {
     imports = [ (modulesPath + "/profiles/all-hardware.nix") ];
@@ -118,9 +75,13 @@ let
 
   # Packages that should be fully linked into /usr,
   # (not just their bin/* files).
+  #
+  # kmod.lib is dlopen()ed by systemd-udevd via libsystemd-shared.so.
+  # It doesn't get picked up from libsystemd-shared.so's RUNPATH due to
+  # https://inbox.vuxu.org/musl/20251017-dlopen-use-rpath-of-caller-dso-v1-1-46c69eda1473@iscas.ac.cn/
   usrPackages = [
     appvm kernel.modules firmware netvm
-  ] ++ (with pkgsGui; [ mesa dejavu_fonts westonLite ]);
+  ] ++ (with pkgsGui; [ dejavu_fonts kmod.lib mesa westonLite ]);
 
   appvms = {
     appvm-firefox = callSpectrumPackage ../../vm/app/firefox.nix {};

host/rootfs/Makefile
@@ -12,6 +12,7 @@ DIRS = \
 	dev \
 	etc/s6-linux-init/env \
 	etc/s6-linux-init/run-image/configs \
+	etc/s6-linux-init/run-image/sd-notify-wrapper \
 	etc/s6-linux-init/run-image/service/dbus/instance \
 	etc/s6-linux-init/run-image/service/dbus/instances \
 	etc/s6-linux-init/run-image/service/dbus/template/data \
@@ -36,7 +37,6 @@ DIRS = \
 	etc/s6-linux-init/run-image/user \
 	etc/s6-linux-init/run-image/vm/by-id \
 	etc/s6-linux-init/run-image/vm/by-name \
-	etc/s6-linux-init/run-image/wait \
 	ext \
 	proc \
 	run \

host/rootfs/file-list.mk
@@ -7,10 +7,6 @@ FILES = \
 	image/etc/group \
 	image/etc/init \
 	image/etc/login \
-	image/etc/mdev.conf \
-	image/etc/mdev/listen \
-	image/etc/mdev/net/add \
-	image/etc/mdev/wait \
 	image/etc/parse-devname \
 	image/etc/passwd \
 	image/etc/s6-linux-init/env/WAYLAND_DISPLAY \
@@ -46,6 +42,7 @@ FILES = \
 	image/etc/s6-linux-init/run-image/service/xdg-desktop-portal-spectrum-host/template/notification-fd \
 	image/etc/s6-linux-init/run-image/service/xdg-desktop-portal-spectrum-host/template/run \
 	image/etc/s6-linux-init/scripts/rc.init \
+	image/etc/udev/rules.d/99-spectrum.rules \
 	image/etc/xdg/weston/autolaunch \
 	image/etc/xdg/weston/weston.ini \
 	image/usr/bin/assign-devices \
@@ -57,6 +54,7 @@ FILES = \
 	image/usr/bin/vm-start \
 	image/usr/bin/vm-stop \
 	image/usr/bin/xdg-open \
+	image/usr/libexec/net-add \
 	image/usr/share/dbus-1/services/org.freedesktop.portal.Documents.service
 
 LINKS = \
@@ -64,24 +62,14 @@ LINKS = \
 	image/etc/s6-linux-init/run-image/opengl-driver \
 	image/etc/s6-linux-init/run-image/service/vmm/template/run \
 	image/lib \
-	image/sbin
+	image/sbin \
+	image/usr/bin/systemd-udevd
 
 S6_RC_FILES = \
-	image/etc/s6-rc/card0/type \
-	image/etc/s6-rc/card0/up \
 	image/etc/s6-rc/core/type \
 	image/etc/s6-rc/core/up \
-	image/etc/s6-rc/kvm/timeout-up \
-	image/etc/s6-rc/kvm/type \
-	image/etc/s6-rc/kvm/up \
-	image/etc/s6-rc/mdevd-coldplug/dependencies.d/mdevd \
-	image/etc/s6-rc/mdevd-coldplug/type \
-	image/etc/s6-rc/mdevd-coldplug/up \
-	image/etc/s6-rc/mdevd/notification-fd \
-	image/etc/s6-rc/mdevd/run \
-	image/etc/s6-rc/mdevd/type \
-	image/etc/s6-rc/ok-all/contents.d/mdevd-coldplug \
 	image/etc/s6-rc/ok-all/contents.d/sys-vmms \
+	image/etc/s6-rc/ok-all/contents.d/systemd-udevd-coldplug \
 	image/etc/s6-rc/ok-all/contents.d/vm-env \
 	image/etc/s6-rc/ok-all/type \
 	image/etc/s6-rc/static-nodes/type \
@@ -89,14 +77,21 @@ S6_RC_FILES = \
 	image/etc/s6-rc/sys-vmms/dependencies.d/vmm-env \
 	image/etc/s6-rc/sys-vmms/type \
 	image/etc/s6-rc/sys-vmms/up \
+	image/etc/s6-rc/systemd-udevd-coldplug/dependencies.d/systemd-udevd \
+	image/etc/s6-rc/systemd-udevd-coldplug/type \
+	image/etc/s6-rc/systemd-udevd-coldplug/up \
+	image/etc/s6-rc/systemd-udevd/notification-fd \
+	image/etc/s6-rc/systemd-udevd/run \
+	image/etc/s6-rc/systemd-udevd/type \
 	image/etc/s6-rc/vm-env/contents.d/static-nodes \
+	image/etc/s6-rc/vm-env/contents.d/systemd-udevd-coldplug \
 	image/etc/s6-rc/vm-env/contents.d/weston \
 	image/etc/s6-rc/vm-env/type \
 	image/etc/s6-rc/vmm-env/contents.d/core \
-	image/etc/s6-rc/vmm-env/contents.d/kvm \
 	image/etc/s6-rc/vmm-env/contents.d/static-nodes \
+	image/etc/s6-rc/vmm-env/contents.d/systemd-udevd-coldplug \
 	image/etc/s6-rc/vmm-env/type \
-	image/etc/s6-rc/weston/dependencies.d/card0 \
+	image/etc/s6-rc/weston/dependencies.d/systemd-udevd-coldplug \
 	image/etc/s6-rc/weston/notification-fd \
 	image/etc/s6-rc/weston/run \
 	image/etc/s6-rc/weston/type

host/rootfs/image/etc/mdev.conf
@@ -1,7 +1,0 @@
-# SPDX-License-Identifier: EUPL-1.2+
-# SPDX-FileCopyrightText: 2021-2022, 2024 Alyssa Ross <hi@alyssa.is>
-
-$PCI_CLASS=^2....$ 0:0 660 +/etc/mdev/net/add
--$MODALIAS=.* 0:0 660 +importas -Siu MODALIAS modprobe -q $MODALIAS
-kvm 0:0 660 +background { /etc/mdev/listen kvm }
-dri/card0 0:0 660 +background { /etc/mdev/listen card0 }

host/rootfs/image/etc/mdev/listen
@@ -1,11 +1,0 @@
-#!/bin/execlineb -S1
-# SPDX-License-Identifier: EUPL-1.2+
-# SPDX-FileCopyrightText: 2021 Alyssa Ross <hi@alyssa.is>
-
-foreground {
-  redirfd -w 2 /dev/null
-  mkfifo /run/wait/${1}
-}
-
-redirfd -w 1 /run/wait/${1}
-echo

host/rootfs/image/etc/mdev/wait
@@ -1,14 +1,0 @@
-#!/bin/execlineb -S1
-# SPDX-License-Identifier: EUPL-1.2+
-# SPDX-FileCopyrightText: 2021 Alyssa Ross <hi@alyssa.is>
-
-foreground {
-  redirfd -w 2 /dev/null
-  mkfifo /run/wait/${1}
-}
-
-foreground {
-  redirfd -w 1 /dev/null
-  head -1 /run/wait/${1}
-}
-rm /run/wait/${1}

host/rootfs/image/etc/s6-rc/card0/type.license
@@ -1,2 +1,0 @@
-SPDX-License-Identifier: CC0-1.0
-SPDX-FileCopyrightText: 2021 Alyssa Ross <hi@alyssa.is>

host/rootfs/image/etc/s6-rc/card0/up
@@ -1,4 +1,0 @@
-# SPDX-License-Identifier: EUPL-1.2+
-# SPDX-FileCopyrightText: 2021 Alyssa Ross <hi@alyssa.is>
-
-/etc/mdev/wait card0

commit 1afc3a7042ee1c40b6d2e564219be31ea8f1017f

host/rootfs: install lvm2
This gives us udev rules necessary for cryptsetup.

Fixes: ced08f7e ("host/rootfs: Switch to systemd-udevd")
Signed-off-by: Alyssa Ross <hi@alyssa.is>

host/rootfs/default.nix
@@ -12,8 +12,8 @@ pkgsMusl.callPackage (
 , lib, stdenvNoCC, nixos, runCommand, writeClosure, erofs-utils, s6-rc
 , btrfs-progs, bubblewrap, busybox, cloud-hypervisor, cosmic-files
 , crosvm, cryptsetup, dejavu_fonts, dbus, execline, foot, fuse3
-, iproute2, inotify-tools, jq, kmod, mdevd, mesa, mount-flatpak, s6
-, s6-linux-init, shadow, socat, systemd, util-linuxMinimal, virtiofsd
+, iproute2, inotify-tools, jq, kmod, lvm2, mdevd, mesa, mount-flatpak
+, s6, s6-linux-init, shadow, socat, systemd, util-linuxMinimal, virtiofsd
 , westonLite, xdg-desktop-portal, xdg-desktop-portal-gtk
 , xdg-desktop-portal-spectrum-host
 }:
@@ -61,8 +61,8 @@ let
   # It doesn't get picked up from libsystemd-shared.so's RUNPATH due to
   # https://inbox.vuxu.org/musl/20251017-dlopen-use-rpath-of-caller-dso-v1-1-46c69eda1473@iscas.ac.cn/
   usrPackages = [
-    appvm dejavu_fonts firmware kernel.modules kmod.lib mesa netvm
-    systemd westonLite
+    appvm dejavu_fonts firmware kernel.modules kmod.lib lvm2 mesa
+    netvm systemd westonLite
   ];
 
   appvms = {