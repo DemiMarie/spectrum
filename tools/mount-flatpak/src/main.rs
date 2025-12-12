DemiMarie/spectrum

commit 76a93435ba221f5f111a1205fc3b22f97f5bff8c

tools/mount-flatpak: init
I tried to build this with Meson, but pathrs depends on thiserror,
which tries to read OUT_DIR, and support for setting that isn't
currently in a Meson release, and requires nightly Rust, so build with
Cargo for now and we can revisit that later.

Link: https://github.com/mesonbuild/meson/pull/15148
Link: https://github.com/rust-lang/rust/issues/118372
Signed-off-by: Alyssa Ross <hi@alyssa.is>
Message-ID: <20251201170458.4186683-7-hi@alyssa.is>
Reviewed-by: Demi Marie Obenour <demiobenour@gmail.com>

tools/mount-flatpak/src/main.rs
@@ -1,0 +1,288 @@
+// SPDX-FileCopyrightText: 2025 Alyssa Ross <hi@alyssa.is>
+// SPDX-License-Identifier: EUPL-1.2+
+
+//! Flatpak installations look like this:
+//!
+//! ```text
+//! flatpak/
+//! ├── app/
+//! │   └── org.gnome.TextEditor/
+//! │       ├── current -> x86_64/stable
+//! │       └── x86_64/
+//! │           └── stable/
+//! │               ├── 0029140121b39f5b7cf4d44fd46b0708eee67f395b5e1291628612a0358fb909/
+//! │               │   └── …
+//! │               └── active -> 0029140121b39f5b7cf4d44fd46b0708eee67f395b5e1291628612a0358fb909
+//! ├── db/
+//! ├── exports/
+//! │   └── …
+//! ├── repo
+//! │   ├── config
+//! │   ├── objects
+//! │   ├── tmp
+//! │   │   └── cache
+//! │   │       └── …
+//! │   └── …
+//! └── runtime
+//!     ├── org.gnome.Platform
+//!     │   └── x86_64
+//!     │       └── 49
+//!     │           ├── active -> bf6aa432cb310726f4ac0ec08cc88558619e1d4bd4b964e27e95187ecaad5400
+//!     │           └── bf6aa432cb310726f4ac0ec08cc88558619e1d4bd4b964e27e95187ecaad5400
+//!     │               └── …
+//!     └── …
+//! ```
+//!
+//! The purpose of this program is to use bind mounts to construct a
+//! Flatpak installation containing only a single application and
+//! runtime, which can be passed through to a VM without exposing
+//! other installed applications.
+
+mod keyfile;
+mod metadata;
+
+use std::borrow::Cow;
+use std::env::{ArgsOs, args_os};
+use std::ffi::OsStr;
+use std::io;
+use std::os::unix::prelude::*;
+use std::path::{Path, PathBuf};
+use std::process::exit;
+
+use pathrs::Root;
+use pathrs::flags::{OpenFlags, ResolverFlags};
+use rustix::fs::{CWD, FileType, fstat};
+use rustix::mount::{MoveMountFlags, OpenTreeFlags, move_mount, open_tree};
+
+use metadata::extract_runtime;
+
+fn ex_usage() -> ! {
+    eprintln!("Usage: mount-flatpak userdata installation app");
+    exit(1);
+}
+
+fn mount_commit(
+    source_commit: &dyn AsFd,
+    target_installation: &Root,
+    path: &Path,
+) -> Result<(), String> {
+    let source_commit_tree = open_tree(
+        source_commit,
+        "",
+        OpenTreeFlags::AT_EMPTY_PATH
+            | OpenTreeFlags::OPEN_TREE_CLONE
+            | OpenTreeFlags::OPEN_TREE_CLOEXEC
+            | OpenTreeFlags::AT_RECURSIVE,
+    )
+    .map_err(|e| format!("cloning source commit tree: {e}"))?;
+    let target_commit_dir = target_installation
+        .mkdir_all(path, &PermissionsExt::from_mode(0o700))
+        .map_err(|e| format!("creating target commit directory: {e}"))?;
+    move_mount(
+        source_commit_tree,
+        "",
+        target_commit_dir,
+        "",
+        MoveMountFlags::MOVE_MOUNT_F_EMPTY_PATH | MoveMountFlags::MOVE_MOUNT_T_EMPTY_PATH,
+    )
+    .map_err(|e| format!("mounting commit: {e}"))
+}
+
+fn run(mut args: ArgsOs) -> Result<(), String> {
+    let Some(user_data_path) = args.next().map(PathBuf::from) else {
+        ex_usage();
+    };
+    let Some(installation_path) = args.next().map(PathBuf::from) else {
+        ex_usage();
+    };
+    let Some(app) = args.next() else {
+        ex_usage();
+    };
+    if args.next().is_some() {
+        ex_usage();
+    }
+
+    let mut user_data =
+        Root::open(&user_data_path).map_err(|e| format!("opening user data partition: {e}"))?;
+    user_data.set_resolver_flags(ResolverFlags::NO_SYMLINKS);
+
+    let mut source_installation_dir = user_data
+        .open_subpath(&installation_path, OpenFlags::O_PATH)
+        .map(Root::from_fd)
+        .map_err(|e| format!("opening source flatpak installation: {e}"))?;
+    source_installation_dir.set_resolver_flags(ResolverFlags::NO_SYMLINKS);
+
+    std::fs::create_dir("flatpak")
+        .map_err(|e| format!("creating target flatpak installation: {e}"))?;
+
+    let target_installation_dir = open_tree(
+        CWD,
+        "flatpak",
+        OpenTreeFlags::OPEN_TREE_CLONE
+            | OpenTreeFlags::OPEN_TREE_CLOEXEC
+            | OpenTreeFlags::AT_RECURSIVE
+            | OpenTreeFlags::AT_SYMLINK_NOFOLLOW,
+    )
+    .map_err(|e| format!("opening target flatpak installation: {e}"))?;
+    let mut target_installation_dir = Root::from_fd(target_installation_dir);
+    target_installation_dir.set_resolver_flags(ResolverFlags::NO_SYMLINKS);
+
+    let mut full_app_path = installation_path.join("app");
+    full_app_path.push(&app);
+    full_app_path.push("current");
+    let arch_and_branch = source_installation_dir
+        .readlink(&full_app_path)
+        .map_err(|e| format!("reading current app arch and branch: {e}"))?;
+    let mut components = arch_and_branch.components();
+    let arch = components.next().unwrap().as_os_str();
+    let branch = components.as_path().as_os_str();
+    if branch.is_empty() {
+        return Err("can't infer branch from \"current\" link".to_string());
+    }
+
+    full_app_path.pop();
+    full_app_path.push(&arch_and_branch);
+    full_app_path.push("active");
+    let commit = source_installation_dir
+        .readlink(&full_app_path)
+        .map_err(|e| format!("reading active app commit: {e}"))?
+        .into_os_string();
+
+    full_app_path.pop();
+    full_app_path.push(&commit);
+    let source_app_dir = source_installation_dir
+        .resolve(&full_app_path)
+        .map_err(|e| format!("opening source app directory: {e}"))?;
+
+    let metadata = source_installation_dir
+        .resolve(full_app_path.join("metadata"))
+        .map_err(|e| format!("resolving app metadata: {e}"))?;
+
+    let metadata_stat =
+        fstat(&metadata).map_err(|e| format!("checking app metadata is a regular file: {e}"))?;
+    let metadata_type = FileType::from_raw_mode(metadata_stat.st_mode);
+    if !metadata_type.is_file() {
+        let e = format!("type of app metadata is {metadata_type:?}, not RegularFile");
+        return Err(e);
+    }
+    let metadata = metadata
+        .reopen(OpenFlags::O_RDONLY)
+        .map_err(|e| format!("opening app metadata: {e}"))?;
+
+    let runtime =
+        extract_runtime(metadata).map_err(|e| format!("reading runtime from metadata: {e}"))?;
+
+    let mut full_runtime_path = installation_path.join("runtime");
+    full_runtime_path.push(runtime);
+    full_runtime_path.push("active");
+    let runtime_commit = source_installation_dir
+        .readlink(&full_runtime_path)
+        .map_err(|e| format!("reading active runtime commit: {e}"))?
+        .into_os_string();
+
+    full_runtime_path.pop();
+    full_runtime_path.push(&runtime_commit);
+    let source_runtime_dir = source_installation_dir
+        .resolve(&full_runtime_path)
+        .map_err(|e| format!("opening source runtime directory: {e}"))?;
+
+    mount_commit(&source_app_dir, &target_installation_dir, &full_app_path)?;
+    mount_commit(
+        &source_runtime_dir,
+        &target_installation_dir,
+        &full_runtime_path,
+    )?;
+
+    target_installation_dir
+        .mkdir_all("repo/objects", &PermissionsExt::from_mode(0o700))
+        .map_err(|e| format!("creating repo/objects: {e}"))?;
+    target_installation_dir
+        .mkdir_all("repo/tmp/cache", &PermissionsExt::from_mode(0o700))
+        .map_err(|e| format!("creating repo/tmp/cache: {e}"))?;
+    let config_target = target_installation_dir
+        .create_file(
+            "repo/config",
+            OpenFlags::O_WRONLY | OpenFlags::O_CLOEXEC,
+            &PermissionsExt::from_mode(0o700),
+        )
+        .map_err(|e| format!("creating repo/config: {e}"))?;
+    let config_source_path = env!("MOUNT_FLATPAK_CONFIG_PATH");
+    let config_source = open_tree(
+        CWD,
+        config_source_path,
+        OpenTreeFlags::OPEN_TREE_CLONE | OpenTreeFlags::OPEN_TREE_CLOEXEC,
+    )
+    .map_err(|e| format!("opening {config_source_path}: {e}"))?;
+    move_mount(
+        config_source,
+        "",
+        config_target,
+        "",
+        MoveMountFlags::MOVE_MOUNT_F_EMPTY_PATH | MoveMountFlags::MOVE_MOUNT_T_EMPTY_PATH,
+    )
+    .map_err(|e| format!("mounting config: {e}"))?;
+
+    let mut attr = libc::mount_attr {
+        attr_clr: libc::MOUNT_ATTR_NOSYMFOLLOW,
+        attr_set: libc::MOUNT_ATTR_RDONLY | libc::MOUNT_ATTR_NODEV,
+        propagation: libc::MS_SLAVE,
+        userns_fd: 0,
+    };
+    let empty = b"\0";
+    // SAFETY: we pass a valid FD, valid C string, and a valid mutable pointer with the
+    // correct size.
+    unsafe {
+        let r = libc::syscall(
+            libc::SYS_mount_setattr,
+            target_installation_dir.as_fd().as_raw_fd() as libc::c_long,
+            empty.as_ptr() as *const libc::c_char,
+            (libc::AT_EMPTY_PATH | libc::AT_RECURSIVE) as libc::c_long,
+            &mut attr as *mut libc::mount_attr,
+            size_of::<libc::mount_attr>() as libc::c_long,
+        );
+        if r == -1 {
+            return Err(format!(
+                "setting target mount attributes: {}",
+                io::Error::last_os_error()
+            ));
+        }
+    }
+    move_mount(
+        target_installation_dir,
+        "",
+        CWD,
+        "flatpak",
+        MoveMountFlags::MOVE_MOUNT_F_EMPTY_PATH,
+    )
+    .map_err(|e| format!("mounting target installation dir: {e}"))?;
+
+    std::fs::create_dir("params").map_err(|e| format!("creating params directory: {e}"))?;
+    std::fs::write("params/id", app.as_bytes()).map_err(|e| format!("writing params/id: {e}"))?;
+    std::fs::write("params/commit", commit.as_bytes())
+        .map_err(|e| format!("writing params/commit: {e}"))?;
+    std::fs::write("params/arch", arch.as_bytes())
+        .map_err(|e| format!("writing params/arch: {e}"))?;
+    std::fs::write("params/branch", branch.as_bytes())
+        .map_err(|e| format!("writing params/branch: {e}"))?;
+    std::fs::write("params/runtime-commit", runtime_commit.as_bytes())
+        .map_err(|e| format!("writing params/runtime-commit: {e}"))?;
+
+    Ok(())
+}
+
+fn main() {
+    let mut args = args_os();
+
+    let prog_name = args
+        .next()
+        .as_ref()
+        .map(Path::new)
+        .and_then(Path::file_name)
+        .map_or(Cow::Borrowed("mount-flatpak"), OsStr::to_string_lossy)
+        .into_owned();
+
+    if let Err(e) = run(args) {
+        eprintln!("{prog_name}: {e}");
+        exit(1);
+    }
+}

pkgs/default.nix
@@ -36,6 +36,7 @@ let
       path: (import path { inherit (self) callPackage; }).override;
 
     rootfs = self.callSpectrumPackage ../host/rootfs {};
+    mount-flatpak = self.callSpectrumPackage ../tools/mount-flatpak {};
     spectrum-build-tools = self.callSpectrumPackage ../tools {
       appSupport = false;
       buildSupport = true;

tools/mount-flatpak/Cargo.lock.license
@@ -1,0 +1,2 @@
+SPDX-License-Identifier: CC0-1.0
+SPDX-FileCopyrightText: 2024 Alyssa Ross <hi@alyssa.is>

tools/mount-flatpak/Cargo.toml
@@ -1,0 +1,11 @@
+# SPDX-License-Identifier: CC0-1.0
+# SPDX-FileCopyrightText: 2025 Alyssa Ross <hi@alyssa.is>
+
+[package]
+name = "mount-flatpak"
+edition = "2024"
+
+[dependencies]
+libc = "0.2.177"
+pathrs = "0.2.1"
+rustix = "1.1.2"

tools/mount-flatpak/config
@@ -1,0 +1,7 @@
+# SPDX-License-Identifier: CC0-1.0
+# SPDX-FileCopyrightText: 2025 Alyssa Ross <hi@alyssa.is>
+
+[core]
+repo_version=1
+mode=bare-user-only
+min-free-space-size=500MB

tools/mount-flatpak/default.nix
@@ -1,0 +1,25 @@
+# SPDX-FileCopyrightText: 2024-2025 Alyssa Ross <hi@alyssa.is>
+# SPDX-License-Identifier: MIT
+
+import ../../lib/call-package.nix (
+{ src, lib, rustPlatform }:
+
+rustPlatform.buildRustPackage {
+  name = "mount-flatpak";
+
+  src = lib.fileset.toSource {
+    root = ../..;
+    fileset = lib.fileset.intersection src ./.;
+  };
+  sourceRoot = "source/tools/mount-flatpak";
+
+  cargoLock.lockFile = ./Cargo.lock;
+
+  env = {
+    MOUNT_FLATPAK_CONFIG_PATH = "${placeholder "out"}/share/spectrum/flatpak-config";
+  };
+
+  postInstall = ''
+    install -Dm 0755 config $MOUNT_FLATPAK_CONFIG_PATH
+  '';
+}) (_: {})

tools/mount-flatpak/src/keyfile.rs
@@ -1,0 +1,189 @@
+// Copyright 2021 System76 <info@system76.com>
+// SPDX-FileCopyrightText: 2025 Alyssa Ross <hi@alyssa.is>
+// SPDX-License-Identifier: MPL-2.0
+
+// Adapted from freedesktop-desktop-entry 0.7.19.
+
+use std::collections::BTreeMap;
+use std::fmt::{self, Display, Formatter};
+
+type Group = BTreeMap<String, String>;
+
+#[derive(Debug)]
+pub enum DecodeError {
+    KeyValueWithoutAGroup,
+    InvalidKey,
+    InvalidValue,
+}
+
+impl Display for DecodeError {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        match self {
+            Self::KeyValueWithoutAGroup => write!(f, "key/value without a group"),
+            Self::InvalidKey => write!(f, "invalid key"),
+            Self::InvalidValue => write!(f, "invalid value"),
+        }
+    }
+}
+
+pub fn parse(input: &str) -> Result<BTreeMap<String, Group>, DecodeError> {
+    let mut groups = BTreeMap::default();
+    let mut active_group: Option<ActiveGroup> = None;
+    let mut active_keys: Option<ActiveKeys> = None;
+
+    for line in input.lines() {
+        process_line(line, &mut groups, &mut active_group, &mut active_keys)?;
+    }
+
+    if let Some(active_keys) = active_keys.take() {
+        match &mut active_group {
+            Some(active_group) => {
+                active_group
+                    .group
+                    .insert(active_keys.key_name, active_keys.value);
+            }
+            None => return Err(DecodeError::KeyValueWithoutAGroup),
+        }
+    }
+
+    if let Some(mut group) = active_group.take() {
+        groups
+            .entry(group.group_name)
+            .or_default()
+            .append(&mut group.group);
+    }
+
+    Ok(groups)
+}
+
+struct ActiveGroup {
+    group_name: String,
+    group: Group,
+}
+
+struct ActiveKeys {
+    key_name: String,
+    value: String,
+}
+
+#[inline(never)]
+fn process_line(
+    line: &str,
+    groups: &mut BTreeMap<String, Group>,
+    active_group: &mut Option<ActiveGroup>,
+    active_keys: &mut Option<ActiveKeys>,
+) -> Result<(), DecodeError> {
+    if line.trim().is_empty() || line.starts_with('#') {
+        return Ok(());
+    }
+
+    let line_bytes = line.as_bytes();
+
+    // if group
+    if line_bytes[0] == b'[' {
+        if let Some(end) = line_bytes[1..].iter().rposition(|&b| b == b']') {
+            let group_name = &line[1..end + 1];
+
+            if let Some(active_keys) = active_keys.take() {
+                match active_group {
+                    Some(active_group) => {
+                        active_group
+                            .group
+                            .insert(active_keys.key_name, active_keys.value);
+                    }
+                    None => return Err(DecodeError::KeyValueWithoutAGroup),
+                }
+            }
+
+            if let Some(mut group) = active_group.take() {
+                groups
+                    .entry(group.group_name)
+                    .or_default()
+                    .append(&mut group.group);
+            }
+
+            active_group.replace(ActiveGroup {
+                group_name: group_name.to_string(),
+                group: Group::default(),
+            });
+        }
+    }
+    // else, if value
+    else if let Some(delimiter) = line_bytes.iter().position(|&b| b == b'=') {
+        let key = &line[..delimiter];
+        let value = format_value(&line[delimiter + 1..])?;
+
+        if key.is_empty() {
+            return Err(DecodeError::InvalidKey);
+        }
+
+        if let Some(active_keys) = active_keys.take() {
+            match active_group {
+                Some(active_group) => {
+                    active_group
+                        .group
+                        .insert(active_keys.key_name, active_keys.value);
+                }
+                None => return Err(DecodeError::KeyValueWithoutAGroup),
+            }
+        }
+        active_keys.replace(ActiveKeys {
+            key_name: key.trim().to_string(),
+            value,
+        });
+    }
+    Ok(())
+}
+
+// https://specifications.freedesktop.org/desktop-entry-spec/latest/value-types.html
+#[inline]
+fn format_value(input: &str) -> Result<String, DecodeError> {
+    let input = if let Some(input) = input.strip_prefix(" ") {
+        input
+    } else {
+        input
+    };
+
+    let mut res = String::with_capacity(input.len());
+
+    let mut last: usize = 0;
+
+    for (i, v) in input.as_bytes().iter().enumerate() {
+        if *v != b'\\' {
+            continue;
+        }
+
+        // edge case for //
+        if last > i {
+            continue;
+        }
+
+        // when there is an \ at the end
+        if input.len() <= i + 1 {
+            return Err(DecodeError::InvalidValue);
+        }
+
+        if last < i {
+            res.push_str(&input[last..i]);
+        }
+
+        last = i + 2;
+
+        match input.as_bytes()[i + 1] {
+            b's' => res.push(' '),
+            b'n' => res.push('\n'),
+            b't' => res.push('\t'),
+            b'r' => res.push('\r'),
+            b'\\' => res.push('\\'),
+            _ => {
+                return Err(DecodeError::InvalidValue);
+            }
+        }
+    }
+
+    if last < input.len() {
+        res.push_str(&input[last..input.len()]);
+    }
+
+    Ok(res)
+}

tools/mount-flatpak/src/metadata.rs
@@ -1,0 +1,19 @@
+// SPDX-License-Identifier: EUPL-1.2+
+// SPDX-FileCopyrightText: 2025 Alyssa Ross <hi@alyssa.is>
+
+use std::fs::File;
+use std::io::read_to_string;
+
+use crate::keyfile::parse;
+
+pub fn extract_runtime(mut metadata: File) -> Result<String, String> {
+    let metadata = read_to_string(&mut metadata).map_err(|e| e.to_string())?;
+    let group = parse(&metadata).map_err(|e| e.to_string())?;
+    let application = group
+        .get("Application")
+        .ok_or_else(|| "Application group missing".to_string())?;
+    Ok(application
+        .get("runtime")
+        .ok_or_else(|| "runtime property missing".to_string())?
+        .clone())
+}

commit ebf6a7a64669ba55b7b9c39ae0f3442754bc534f

tools/mount-flatpak: fix double installation path
I've mostly been testing this with an installation path of ".", at the
root of the filesystem, so I'd missed that we were prepending the
installation path even though we were also opening from a Root at the
installation path.

Fixes: 76a93435 ("tools/mount-flatpak: init")
Signed-off-by: Alyssa Ross <hi@alyssa.is>

tools/mount-flatpak/src/main.rs
@@ -127,7 +127,7 @@ fn run(mut args: ArgsOs) -> Result<(), String> {
     let mut target_installation_dir = Root::from_fd(target_installation_dir);
     target_installation_dir.set_resolver_flags(ResolverFlags::NO_SYMLINKS);
 
-    let mut full_app_path = installation_path.join("app");
+    let mut full_app_path = PathBuf::from("app");
     full_app_path.push(&app);
     full_app_path.push("current");
     let arch_and_branch = source_installation_dir
@@ -172,7 +172,7 @@ fn run(mut args: ArgsOs) -> Result<(), String> {
     let runtime =
         extract_runtime(metadata).map_err(|e| format!("reading runtime from metadata: {e}"))?;
 
-    let mut full_runtime_path = installation_path.join("runtime");
+    let mut full_runtime_path = PathBuf::from("runtime");
     full_runtime_path.push(runtime);
     full_runtime_path.push("active");
     let runtime_commit = source_installation_dir