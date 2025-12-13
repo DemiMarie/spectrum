DemiMarie/spectrum

commit aa5ab817d6eb990683a88893a6375acef7a585aa

Upgrade to Rust 2024
Signed-off-by: Alyssa Ross <hi@alyssa.is>

tools/xdg-desktop-portal-spectrum-host/src/main.rs
@@ -19,7 +19,7 @@ use async_executor::StaticExecutor;
 use async_io::Async;
 use futures_lite::prelude::*;
 use futures_lite::stream::StreamExt;
-use zbus::{connection, AuthMechanism, Connection, MessageStream};
+use zbus::{AuthMechanism, Connection, MessageStream, connection};
 
 use file_chooser::FileChooser;
 

release/checks/rustfmt.nix
@@ -13,6 +13,6 @@ runCommand "spectrum-rustfmt" {
   nativeBuildInputs = [ rustfmt ];
 } ''
   shopt -s globstar
-  rustfmt --check --edition 2018 $src/**/*.rs
+  rustfmt --check --edition 2024 $src/**/*.rs
   touch $out
 '') (_: {})

tools/lsvm.rs
@@ -6,13 +6,13 @@ use std::collections::HashMap;
 use std::env::args_os;
 use std::ffi::{OsStr, OsString};
 use std::fs::read_dir;
-use std::io::{self, stdout, Write};
+use std::io::{self, Write, stdout};
 use std::os::unix::prelude::*;
 use std::path::Path;
-use std::process::{exit, Command, Stdio};
+use std::process::{Command, Stdio, exit};
 use std::str;
 
-use miniserde::{json, Deserialize};
+use miniserde::{Deserialize, json};
 
 fn prog_name() -> String {
     args_os()

tools/meson.build
@@ -4,7 +4,7 @@
 project('spectrum-tools', 'c',
   default_options : {
     'c_std': 'c23',
-    'rust_std': '2018',
+    'rust_std': '2024',
     'warning_level': '3',
   })
 

tools/start-vmm/ch.rs
@@ -12,7 +12,7 @@ use std::path::Path;
 use std::process::{Command, Stdio};
 use std::string::FromUtf8Error;
 
-use miniserde::{json, Serialize};
+use miniserde::{Serialize, json};
 
 use crate::net::MacAddress;
 use crate::s6::notify_readiness;
@@ -148,10 +148,10 @@ pub fn add_net(vm_dir: &Path, net: &NetConfig) -> Result<(), NonZeroI32> {
         .spawn()
         .or(Err(EPERM))?;
 
-    if let Ok(ch_remote_status) = ch_remote.wait() {
-        if ch_remote_status.success() {
-            return Ok(());
-        }
+    if let Ok(ch_remote_status) = ch_remote.wait()
+        && ch_remote_status.success()
+    {
+        return Ok(());
     }
 
     Err(EPROTO)

tools/start-vmm/lib.rs
@@ -83,10 +83,14 @@ pub fn vm_config(vm_dir: &Path) -> Result<VmConfig, String> {
         },
         fs: [FsConfig {
             tag: "virtiofs0",
-            socket: format!("/run/service/vm-services/instance/{vm_name}/data/service/vhost-user-fs/env/virtiofsd.sock"),
+            socket: format!(
+                "/run/service/vm-services/instance/{vm_name}/data/service/vhost-user-fs/env/virtiofsd.sock"
+            ),
         }],
         gpu: vec![GpuConfig {
-            socket: format!("/run/service/vm-services/instance/{vm_name}/data/service/vhost-user-gpu/env/crosvm.sock"),
+            socket: format!(
+                "/run/service/vm-services/instance/{vm_name}/data/service/vhost-user-gpu/env/crosvm.sock"
+            ),
         }],
         memory: MemoryConfig {
             size: 1 << 30,

tools/start-vmm/net.rs
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: EUPL-1.2+
-// SPDX-FileCopyrightText: 2022-2024 Alyssa Ross <hi@alyssa.is>
+// SPDX-FileCopyrightText: 2022-2025 Alyssa Ross <hi@alyssa.is>
 
 use std::borrow::Cow;
 use std::ffi::{c_char, c_int};
 use std::fmt::{self, Display, Formatter};
 
-use miniserde::ser::Fragment;
 use miniserde::Serialize;
+use miniserde::ser::Fragment;
 
 use crate::ch::NetConfigC;
 
@@ -36,7 +36,8 @@ impl Serialize for MacAddress {
     }
 }
 
-extern "C" {
+// SAFETY: declaration is compatible with C.
+unsafe extern "C" {
     /// # Safety
     ///
     /// The rest of the result is only valid if the returned fd is not -1.

tools/start-vmm/start-vmm.rs
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: EUPL-1.2+
-// SPDX-FileCopyrightText: 2022-2024 Alyssa Ross <hi@alyssa.is>
+// SPDX-FileCopyrightText: 2022-2025 Alyssa Ross <hi@alyssa.is>
 
 use std::env::args_os;
 use std::fs::File;
@@ -28,7 +28,8 @@ unsafe fn run() -> Result<(), String> {
     }
 
     let vm_dir = Path::new("/run/vm/by-id").join(vm_name);
-    let ready_fd = File::from_raw_fd(3);
+    // SAFETY: invoker promises this FD is valid.
+    let ready_fd = unsafe { File::from_raw_fd(3) };
 
     create_vm(&vm_dir, ready_fd)
 }

commit 3035fbf53c38bde642ec9cffd65dc6568a33183d

tools/xdg-desktop-portal-spectrum-host: rust: 2021 -> 2024
This was missed when everything else was updated.

Fixes: aa5ab817 ("Upgrade to Rust 2024")
Signed-off-by: Alyssa Ross <hi@alyssa.is>

tools/xdg-desktop-portal-spectrum-host/Cargo.toml
@@ -4,7 +4,7 @@
 [package]
 name = "xdg-desktop-portal-spectrum-host"
 version = "0.1.0"
-edition = "2021"
+edition = "2024"
 
 [dependencies]
 async-executor = { version = "1.12.0", features = ["static"] }

tools/xdg-desktop-portal-spectrum-host/src/main.rs
@@ -262,10 +262,10 @@ fn run() -> Result<(), String> {
 }
 
 fn msg(e: &str) {
-    if let Some(prog) = args_os().next() {
-        if let Some(prog) = PathBuf::from(prog).file_name() {
-            eprint!("{}: ", prog.to_string_lossy());
-        }
+    if let Some(prog) = args_os().next()
+        && let Some(prog) = PathBuf::from(prog).file_name()
+    {
+        eprint!("{}: ", prog.to_string_lossy());
     }
     eprintln!("{e}");
 }