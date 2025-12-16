8164c15db6dea50f05c59d9d4f819e07ceb050c9
Demi Marie Obenour <demiobenour@gmail.com>
2025-12-16 03:24:22 -0500

    kernel config
    Signed-off-by: Demi Marie Obenour <demiobenour@gmail.com>

--- host/rootfs/default.nix
+++ host/rootfs/default.nix
@@ -22,7 +22,7 @@ let
   inherit (nixosAllHardware.config.hardware) firmware;
   inherit (lib)
     concatMapStringsSep concatStrings escapeShellArgs fileset mapAttrsToList
-    trivial;
+    mkForce trivial;
 
   packages = [
     btrfs-progs bubblewrap cloud-hypervisor cosmic-files crosvm cryptsetup dbus
@@ -49,8 +49,263 @@ let
     system.stateVersion = trivial.release;
   });
 
-  kernel = linux_latest;
+  kernel = linux_latest.override {
+    structuredExtraConfig = with lib.kernel; {
+      SCSI_PROC_FS = no;
 
+      # No network drivers!
+      NETDEVICES = lib.mkForce no;
+
+      # No IP networking!
+      INET = lib.mkForce no;
+
+      # No Bluetooth
+      BT = lib.mkForce no;
+
+      # No NFC
+      NFC = no;
+
+      # No wireless on the host, please.
+      WIRELESS = lib.mkForce no;
+
+      # No spanning tree.
+      MRP = no;
+      VLAN_8021Q = no;
+
+      # No 9pfs
+      NET_9P = lib.mkForce no;
+
+      # No AppleTalk
+      ATALK = no;
+
+      # No unused filesystems
+      XFS_FS = mkForce no;
+      EXT2_FS = mkForce no;
+      EXT3_FS = mkForce no;
+      EXT4_FS = mkForce no;
+      NTFS_FS = mkForce no;
+      NTFS3_FS = mkForce no;
+      OCFS2_FS = mkForce no;
+      JFS_FS = mkForce no;
+      NFS_FS = mkForce no;
+
+      # No ATM
+      ATM = mkForce no;
+
+      # No mesh networking
+      BATMAN_ADV = mkForce no;
+
+      # No bridging
+      BRIDGE = lib.mkForce no;
+
+      # No CAIF, whatever the heck that is.
+      CAIF = mkForce no;
+
+      # The Spectrum host will not have a CAN bus!
+      CAN = mkForce no;
+
+      # No network filesystems
+      NETWORK_FILESYSTEMS = no;
+
+      # No Data Center Bridging
+      DCB = mkForce no;
+
+      # No DNS resolution
+      DNS_RESOLVER = mkForce no;
+
+      # No hardware switch support
+      NET_DSA = mkForce no;
+
+      # No Ethernet redundancy
+      HSR = mkForce no;
+
+      # No 802.15.4
+      IEEE802154 = mkForce no;
+
+      # No packet sockets
+      PACKET = mkForce no;
+
+      # No 802.2 Logical Link Control
+      LLC = mkForce no;
+
+      # No ForCES
+      NET_IFE = mkForce no;
+
+      # No X.25 LAPB
+      LAPB = mkForce no;
+
+      # No 802.11 protocol layer
+      MAC80211 = lib.mkForce no;
+
+      # No LoWPAN
+      MAC802154 = mkForce no;
+
+      # No Management Component Transport Protocol
+      MCTP = no; # TODO: could someone run Spectrum in a server with a BMC?
+
+      # No Multiprotocol Label Switching
+      MPLS = mkForce no;
+
+      # No firewalling!
+      NETFILTER = lib.mkForce no;
+
+      # No network MAC labeling
+      NETLABEL = mkForce no;
+
+      # No netlink diagnostics
+      NETLINK_DIAG = mkForce no;
+
+      # No network service headers
+      NET_NSH = mkForce no;
+
+      # No OpenVSwitch
+      OPENVSWITCH = mkForce no;
+
+      # No support for Nokia cellular modems
+      PHONET = mkForce no;
+
+      # No Traffic Control sampling
+      PSAMPLE = mkForce no;
+
+      # No Qualcomm IPC Router
+      QRTR = mkForce no;
+
+      # No network scheduler
+      NET_SCHED = mkForce no;
+
+      # No AF_VSOCK.  The user-mode implementation is used on the host.
+      VSOCKETS = mkForce no;
+
+      # No amateur radio.
+      HAMRADIO = mkForce no;
+
+      # No X.25 protocol
+      X25 = mkForce no;
+
+      # No AF_XDP as there are no network devices for it to run on.
+      XDP_SOCKETS = mkForce no;
+
+      # No trivial DMA attacks
+      FIREWIRE = mkForce no;
+      HOTPLUG_PCI_ACPI = mkForce no;
+      HOTPLUG_PCI_PCIE = mkForce no;
+
+      # No 32-bit code
+      IA32_EMULATION = mkForce no;
+      X86_X32_ABI = mkForce no;
+
+      # No NVMe target support
+      NVME_TARGET = mkForce no;
+
+      # No NVMe-oF support
+      NVME_HOST_AUTH = mkForce no;
+      NVME_FC = mkForce no;
+
+      # No Fibre Channel
+      NET_FC = mkForce no;
+
+      # We are a desktop OS.  Enable preemption.
+      PREEMPT = mkForce yes;
+      PREEMPT_VOLUNTARY = mkForce no;
+
+      # Allow it to be overridden on command line.
+      PREEMPT_DYNAMIC = yes;
+
+      # No ROCm userspace on host.  Any host GPU compute would use OpenGL(-ES) or Vulkan.
+      HSA_AMD = mkForce no;
+
+      # No Ethernet drivers
+      ETHERNET = mkForce no;
+
+      # No old code
+      X86_VSYSCALL_EMULATION = mkForce no;
+
+      # No segmented or 32-bit code
+      MODIFY_LDT_SYSCALL = mkForce no;
+
+      # We don't use UEFI secure boot in guests.
+      KVM_SMM = mkForce no;
+
+      # No POSIX message queues
+      POSIX_MQUEUE = mkForce no;
+
+      # No OOB messages on AF_UNIX.  These are only really useful for exploits.
+      AF_UNIX_OOB = mkForce no;
+
+      # No machine learning accelerators.  Spectrum's host has no userspace for them.
+      DRM_ACCEL = mkForce no;
+
+      # No SCSI targets
+      ISCSI_TARGET = mkForce no;
+
+      # Spectrum can't run under Xen (because it needs virtualization).
+      XEN = mkForce no;
+      XEN_BALLOON = mkForce unset;
+      XEN_BALLOON_MEMORY_HOTPLUG = mkForce unset;
+      XEN_DOM0 = mkForce unset;
+      XEN_EFI = mkForce unset;
+      XEN_HAVE_PVMMU = mkForce unset;
+      XEN_PVH = mkForce unset;
+      XEN_PVHVM = mkForce unset;
+      XEN_SAVE_RESTORE = mkForce unset;
+      XEN_SYS_HYPERVISOR = mkForce unset;
+
+      # Unused options from NixOS
+      BONDING = mkForce unset;
+      BPF_STREAM_PARSER = mkForce unset;
+      BRIDGE_VLAN_FILTERING = mkForce unset;
+      BT_HCIUART_QCA = mkForce unset;
+      BT_HCIUART_SERDEV = mkForce unset;
+      BT_HCIBTUSB_AUTOSUSPEND = mkForce unset;
+      BT_HCIBTUSB_MTK = mkForce unset;
+      BT_HCIBTUSB_QCA = mkForce unset;
+      BT_HCIBTUSB_SERDEV = mkForce unset;
+      BT_QCA = mkForce unset;
+      CLS_U32_MARK = mkForce unset;
+      CLS_U32_PERF = mkForce unset;
+
+      # Spectrum does not need to support Xen guests.
+      KVM_XEN = mkForce no;
+
+      # No old-style i915 GVT-g
+      DRM_I915_GVT = mkForce no;
+      DRM_I915_GVT_KVMGT = mkForce no;
+
+      # No RDMA cgroup
+      CGROUP_RDMA = mkForce no;
+
+      # No Windows guests (yet)
+      KVM_HYPERV = mkForce no;
+
+      # Various hardening options.
+      # Crash the kernel if something goes really wrong
+      PANIC_ON_OOPS = yes;
+      BUG_ON_DATA_CORRUPTION = yes;
+      DEBUG_LIST = yes;
+      FORTIFY_SOURCE = yes;
+      HARDENED_USERCOPY = yes;
+      HARDENED_USERCOPY_DEFAULT_ON = yes;
+      HARDENED_LIST_HARDENED = yes;
+      BUG = yes;
+      INIT_ON_FREE_DEFAULT_ON = yes;
+      INIT_ON_ALLOC_DEFAULT_ON = yes;
+      INIT_STACK_ALL_ZERO = yes;
+      ZERO_CALL_USED_REGS = yes;
+
+      # Not used.  Yet.
+      SECURITY_APPARMOR = mkForce no;
+      # Enable VM checks
+      DEBUG_VM = yes;
+
+      # Allow dumping more data to the screen on panics, so that reports from end-users
+      # are better.  The default is meant for developer convenience.
+      #DRM_PANIC_SCREEN = lib.mkForce (freeform "qr_code");
+      DRM_PANIC_SCREEN_QR_CODE = yes;
+
+      # No use for USB gadgets
+      USB_GADGET = mkForce no;
+   };
+  };
   appvm = callSpectrumPackage ../../img/app { inherit (foot) terminfo; };
   netvm = callSpectrumPackage ../../vm/sys/net { inherit (foot) terminfo; };
 
