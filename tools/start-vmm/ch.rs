DemiMarie/spectrum

commit a655117d2226fa92bf2265b79a026696e5ce1fe2

tools: start-vmm: enable Cloud Hypervisor landlock
We can't really predict the device paths or IOMMU groups statically,
so this is as good as it gets with landlock rules.  We'll be able to
do other things to further lock things down though, like running
different Cloud Hypervisor instances as different users, and changing
ownership of each IOMMU group in /dev/vfio/vfio to match.

Message-ID: <20251110184907.346511-1-hi@alyssa.is>
Signed-off-by: Alyssa Ross <hi@alyssa.is>

tools/start-vmm/ch.rs
@@ -70,6 +70,12 @@ pub struct VsockConfig {
 }
 
 #[derive(Serialize)]
+pub struct LandlockConfig {
+    pub path: &'static str,
+    pub access: &'static str,
+}
+
+#[derive(Serialize)]
 pub struct VmConfig {
     pub console: ConsoleConfig,
     pub disks: Vec<DiskConfig>,
@@ -80,6 +86,8 @@ pub struct VmConfig {
     pub payload: PayloadConfig,
     pub serial: ConsoleConfig,
     pub vsock: VsockConfig,
+    pub landlock_enable: bool,
+    pub landlock_rules: Vec<LandlockConfig>,
 }
 
 fn command(vm_dir: &Path, s: impl AsRef<OsStr>) -> Command {

tools/start-vmm/lib.rs
@@ -14,8 +14,8 @@ use std::io::{self, ErrorKind};
 use std::path::Path;
 
 use ch::{
-    ConsoleConfig, DiskConfig, FsConfig, GpuConfig, MemoryConfig, PayloadConfig, VmConfig,
-    VsockConfig,
+    ConsoleConfig, DiskConfig, FsConfig, GpuConfig, LandlockConfig, MemoryConfig, PayloadConfig,
+    VmConfig, VsockConfig,
 };
 use net::net_setup;
 
@@ -130,6 +130,17 @@ pub fn vm_config(vm_dir: &Path) -> Result<VmConfig, String> {
             cid: 3,
             socket: vm_dir.join("vsock").into_os_string().into_string().unwrap(),
         },
+        landlock_enable: true,
+        landlock_rules: vec![
+            LandlockConfig {
+                path: "/sys/devices",
+                access: "rw",
+            },
+            LandlockConfig {
+                path: "/dev/vfio",
+                access: "rw",
+            },
+        ],
     })
 }
 

commit 0f3388f0191d9a03c7bf471c269a34a79f22018b

tools/start-vmm: avoid unnecessary heap use
Very much a micro-optimization, but I noticed it when reading the
code.

Fixes: 090f560 ("host/start-vmm: provide Wayland unconditionally")
Fixes: a655117 ("tools: start-vmm: enable Cloud Hypervisor landlock")
Signed-off-by: Alyssa Ross <hi@alyssa.is>

tools/start-vmm/ch.rs
@@ -74,14 +74,14 @@ pub struct VmConfig {
     pub console: ConsoleConfig,
     pub disks: Vec<DiskConfig>,
     pub fs: [FsConfig; 1],
-    pub gpu: Vec<GpuConfig>,
+    pub gpu: [GpuConfig; 1],
     pub memory: MemoryConfig,
     pub net: Vec<NetConfig>,
     pub payload: PayloadConfig,
     pub serial: ConsoleConfig,
     pub vsock: VsockConfig,
     pub landlock_enable: bool,
-    pub landlock_rules: Vec<LandlockConfig>,
+    pub landlock_rules: [LandlockConfig; 2],
 }
 
 fn command(vm_dir: &Path, s: impl AsRef<OsStr>) -> Command {

tools/start-vmm/lib.rs
@@ -86,7 +86,7 @@ pub fn vm_config(vm_dir: &Path) -> Result<VmConfig, String> {
                 "/run/service/vm-services/instance/{vm_name}/data/service/vhost-user-fs/env/virtiofsd.sock"
             ),
         }],
-        gpu: vec![GpuConfig {
+        gpu: [GpuConfig {
             socket: format!(
                 "/run/service/vm-services/instance/{vm_name}/data/service/vhost-user-gpu/env/crosvm.sock"
             ),
@@ -167,7 +167,7 @@ pub fn vm_config(vm_dir: &Path) -> Result<VmConfig, String> {
             socket: format!("/run/vsock/{vm_name}/vsock"),
         },
         landlock_enable: true,
-        landlock_rules: vec![
+        landlock_rules: [
             LandlockConfig {
                 path: "/sys/devices",
                 access: "rw",