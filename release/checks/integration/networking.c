DemiMarie/spectrum

commit d0dba15c869b52ae49dfb94a7195c3e7b2c972df

release/checks/integration: handle partial sends
Sometimes we see a test failure that looks like this:

	unexpected connection data: qemu-system-aarch64: terminating on signal 15 from pid 184 ()

I think this is because, now that the nc command in the networking
integration test has a timeout, it's possible for it to time out after
having opened the connection, but before having written all its input
to it.  Therefore, ignore connections that send a prefix of the
expected data (including nothing), and just wait for the next
connection rather than failing if that happens.

Closes: https://spectrum-os.org/lists/archives/spectrum-devel/875xbl33vw.fsf@alyssa.is/
Fixes: c61b297 ("release/checks/integration: add nc timeout")
Signed-off-by: Alyssa Ross <hi@alyssa.is>
Message-ID: <20251129180016.3895-2-hi@alyssa.is>
Reviewed-by: Yureka Lilian <yureka@cyberchaos.dev>

release/checks/integration/networking.c
@@ -57,32 +57,44 @@ static int setup_server(void)
 
 static void expect_connection(int listener)
 {
-	int conn_fd;
-	FILE *conn;
+	int conn, r;
 	char msg[7];
 	size_t len;
 
-	fputs("waiting for server connection\n", stderr);
-	if ((conn_fd = accept(listener, nullptr, nullptr)) == -1) {
-		perror("accept");
-		exit(EXIT_FAILURE);
-	}
-	fputs("accepted connection!\n", stderr);
-	if (!(conn = fdopen(conn_fd, "r"))) {
-		perror("fdopen(server connection)");
-		exit(EXIT_FAILURE);
-	}
+	for (;;) {
+		len = 0;
 
-	len = fread(msg, 1, sizeof msg, conn);
-	if (len != 6 || memcmp("hello\n", msg, 6)) {
-		if (ferror(conn))
-			perror("fread(server connection)");
-		else
+		fputs("waiting for server connection\n", stderr);
+		if ((conn = accept(listener, nullptr, nullptr)) == -1) {
+			perror("accept");
+			exit(EXIT_FAILURE);
+		}
+		fputs("accepted connection!\n", stderr);
+
+		for (;;) {
+			r = read(conn, msg + len, sizeof msg - len);
+			if (r == -1) {
+				perror("read");
+				exit(EXIT_FAILURE);
+			}
+			if (r == 0)
+				break;
+			len += r;
+		}
+		close(conn);
+
+		if (memcmp("hello\n", msg, len) || len > 6) {
 			fprintf(stderr, "unexpected connection data: %.*s",
 			        (int)len, msg);
-		exit(EXIT_FAILURE);
+			exit(EXIT_FAILURE);
+		}
+
+		// If connection was disconnect partway through, try again.
+		if (len < 6)
+			continue;
+
+		return;
 	}
-	fclose(conn);
 }
 
 static void drain_connections(int listener)

commit 0e89fc49d69a6405d63f98375f0212e13c6c13b6

release/checks/integration: fix type of variable
Fixes: d0dba15c ("release/checks/integration: handle partial sends")
Signed-off-by: Alyssa Ross <hi@alyssa.is>

release/checks/integration/networking.c
@@ -129,9 +129,10 @@ static int setup_server(void)
 
 static void expect_connection(int listener)
 {
-	int conn, r;
+	int conn;
 	char msg[7];
 	size_t len;
+	ssize_t r;
 
 	for (;;) {
 		len = 0;