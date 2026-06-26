e4562d4c7646fea3cbb7306ff9b7ff41154c75c3
Alyssa Ross <hi@alyssa.is>
2026-06-26 15:34:38 +0200

    pkgs: skawarePackages: use GitHub URLs for patches
    The cgit URL has not been reliable.  Overzealous anti-scraper
    detection or something.  Use the official GitHub mirror instead.
    We'll get rid of these backports soon when we update Nixpkgs anyway.
    
    Link: https://inbox.spectrum-os.org/spectrum-devel/fe5ebc4b6b171af5955cc4c2f256a1006cd79885.1782409229.git.colby@colbyt.com
    Signed-off-by: Alyssa Ross <hi@alyssa.is>

--- pkgs/skaware-packages/default.nix
+++ pkgs/skaware-packages/default.nix
@@ -7,7 +7,7 @@ super.skawarePackages.overrideScope (_: prev: {
   s6 = prev.s6.overrideAttrs ({ patches ? [], ... }: {
     patches = patches ++ [
       (final.fetchpatch {
-        url = "https://git.skarnet.org/cgi-bin/cgit.cgi/s6/patch/?id=c3a8ef7034fb2bc02f35381a8970ac026822a810";
+        url = "https://github.com/skarnet/s6/commit/c3a8ef7034fb2bc02f35381a8970ac026822a810.patch";
         hash = "sha256-lgCoPbEYru6/a2bpVpLsZ2Rq2OHhNVs0lDgFO/df1Aw=";
       })
     ];
@@ -16,7 +16,7 @@ super.skawarePackages.overrideScope (_: prev: {
   mdevd = prev.mdevd.overrideAttrs ({ patches ? [], ... }: {
     patches = patches ++ [
       (final.fetchpatch {
-        url = "https://git.skarnet.org/cgi-bin/cgit.cgi/mdevd/patch/?id=252f241e425bf09ddfb4a824e40403f40da0da1e";
+        url = "https://github.com/skarnet/mdevd/commit/252f241e425bf09ddfb4a824e40403f40da0da1e.patch";
         hash = "sha256-0tEC+yJGyPapsxBqzBXPztF3bl7OwjVAGjhNXtwZQ0g=";
       })
     ];
