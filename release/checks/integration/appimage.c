DemiMarie/spectrum

commit 546305b8a9fb401b4654ab8ab945e748aba4f191

release/checks/integration: avoid multiple prompts
This isn't a problem now, because we don't wait for any prompts after
this, but when we do, we want to make sure we're only prompted after
all the commands have completed, not just the first one.

Signed-off-by: Alyssa Ross <hi@alyssa.is>

release/checks/integration/networking.c
@@ -89,12 +89,12 @@ void test(struct config c)
 
 	struct vm vm = start_qemu(c);
 
-	if (fputs("set -euxo pipefail\n"
-	          "mkdir /run/mnt\n"
-	          "mount \"$(findfs UUID=a7834806-2f82-4faf-8ac4-4f8fd8a474ca)\" /run/mnt\n"
-	          "s6-rc -bu change vmm-env\n"
-	          "vm-import user /run/mnt/vms\n"
-	          "vm-start user.nc\n"
+	if (fputs("set -euxo pipefail && "
+	          "mkdir /run/mnt && "
+	          "mount \"$(findfs UUID=a7834806-2f82-4faf-8ac4-4f8fd8a474ca)\" /run/mnt && "
+	          "s6-rc -bu change vmm-env && "
+	          "vm-import user /run/mnt/vms && "
+	          "vm-start user.nc && "
 	          "tail -Fc +0 /run/log/current /run/*.log\n",
 	          vm.console) == EOF) {
 		fputs("error writing to console\n", stderr);

commit 0adfbcb6c12f35b33b8f47dc527587d4d20eeb93

release/checks/integration: fix false positives
These tests are written to assume that if they see another prompt
after running their commands, those commands have been successful.
That is, they expect the shell to exit and not be restarted if a
command fails.  Only the appimage test actually set the shell not to
be automatically restarted with s6-svc -O, so copy that to the other
tests using the same pattern.

Furthermore, I've just learned that set -e will only cause the shell
to exit if the last command in an AND-OR list fails, not if the left
hand side of an && fails as these tests were written to assume.  This
meant that many command failures would cause the shell to stop
processing, but print a new prompt, also resulting in a false
positive.

Fixes: 546305b ("release/checks/integration: avoid multiple prompts")
Fixes: 514cf5a ("release/checks/integration: add appimage test")
Fixes: f2f6d7d ("release/checks/integration: add portal test")
Signed-off-by: Alyssa Ross <hi@alyssa.is>

release/checks/integration/appimage.c
@@ -11,10 +11,10 @@ void test(struct config c)
 	start_console_thread(vm, "~ # ");
 	wait_for_prompt(vm);
 
-	if (fputs("set -euxo pipefail && "
-	          "s6-svc -O /run/service/serial-getty/instance/* && "
-	          "mkdir /run/mnt && "
-	          "mount \"$(findfs UUID=a7834806-2f82-4faf-8ac4-4f8fd8a474ca)\" /run/mnt && "
+	if (fputs("set -euxo pipefail ; "
+	          "s6-svc -O /run/service/serial-getty/instance/* ; "
+	          "mkdir /run/mnt ; "
+	          "mount \"$(findfs UUID=a7834806-2f82-4faf-8ac4-4f8fd8a474ca)\" /run/mnt ; "
 	          "run-appimage /run/mnt/test.appimage\n",
 	          vm_console_writer(vm)) == EOF) {
 		fputs("error writing to console\n", stderr);

release/checks/integration/networking.c
@@ -201,12 +201,13 @@ void test(struct config c)
 	start_console_thread(vm, "~ # ");
 	wait_for_prompt(vm);
 
-	if (fputs("set -euxo pipefail && "
-	          "mkdir /run/mnt && "
-	          "mount \"$(findfs UUID=a7834806-2f82-4faf-8ac4-4f8fd8a474ca)\" /run/mnt && "
-	          "s6-rc -bu change vmm-env && "
-	          "vm-import user /run/mnt/vms /run/mnt/storage && "
-	          "vm-start \"$(basename \"$(readlink /run/vm/by-name/user.nc)\")\" && "
+	if (fputs("set -euxo pipefail ; "
+	          "s6-svc -O /run/service/serial-getty/instance/* ; "
+	          "mkdir /run/mnt ; "
+	          "mount \"$(findfs UUID=a7834806-2f82-4faf-8ac4-4f8fd8a474ca)\" /run/mnt ; "
+	          "s6-rc -bu change vmm-env ; "
+	          "vm-import user /run/mnt/vms /run/mnt/storage ; "
+	          "vm-start \"$(basename \"$(readlink /run/vm/by-name/user.nc)\")\" ; "
 	          "tail -Fc +0 /run/log/current /run/vm/by-id/*/serial &\n",
 	          vm_console_writer(vm)) == EOF) {
 		fputs("error writing to console\n", stderr);

release/checks/integration/portal.c
@@ -11,15 +11,16 @@ void test(struct config c)
 	start_console_thread(vm, "~ # ");
 	wait_for_prompt(vm);
 
-	if (fputs("set -euxo pipefail && "
-	          "(tail -Fc +0 /run/log/current &) && "
-	          "mkdir /run/mnt && "
-	          "mount \"$(findfs UUID=a7834806-2f82-4faf-8ac4-4f8fd8a474ca)\" /run/mnt && "
-	          "s6-rc -bu change vmm-env && "
-	          "vm-import user /run/mnt/vms /run/mnt/storage && "
-	          "(tail -Fc +0 /run/vm/by-id/*/serial &) && "
-	          "s6-svc -O /run/vm/by-name/user.portal/service && "
-	          "vm-start \"$(basename \"$(readlink /run/vm/by-name/user.portal)\")\" && "
+	if (fputs("set -euxo pipefail ; "
+	          "s6-svc -O /run/service/serial-getty/instance/* ; "
+	          "tail -Fc +0 /run/log/current & "
+	          "mkdir /run/mnt ; "
+	          "mount \"$(findfs UUID=a7834806-2f82-4faf-8ac4-4f8fd8a474ca)\" /run/mnt ; "
+	          "s6-rc -bu change vmm-env ; "
+	          "vm-import user /run/mnt/vms /run/mnt/storage ; "
+	          "tail -Fc +0 /run/vm/by-id/*/serial & "
+	          "s6-svc -O /run/vm/by-name/user.portal/service ; "
+	          "vm-start \"$(basename \"$(readlink /run/vm/by-name/user.portal)\")\" ; "
 	          "s6-svwait -d /run/vm/by-name/user.portal/service\n",
 	          vm_console_writer(vm)) == EOF) {
 		fputs("error writing to console\n", stderr);