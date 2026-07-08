DemiMarie/spectrum

commit f2f6d7d2a36fd37910335654b8fc69fb01934cb3

release/checks/integration: add portal test
This was enough to (sometimes) reproduce the intermittent failure
caused by a Cloud Hypervisor VSOCK bug.

Link: https://github.com/cloud-hypervisor/cloud-hypervisor/commit/ec8fceb4a6a537c4d838287d47edc2c156379b4e
Signed-off-by: Alyssa Ross <hi@alyssa.is>

release/checks/integration/portal.c
@@ -1,0 +1,28 @@
+// SPDX-License-Identifier: EUPL-1.2+
+// SPDX-FileCopyrightText: 2025 Alyssa Ross <hi@alyssa.is>
+
+#include "lib.h"
+
+#include <stdlib.h>
+
+void test(struct config c)
+{
+	struct vm vm = start_qemu(c);
+
+	if (fputs("set -euxo pipefail && "
+	          "(tail -Fc +0 /run/log/current &) && "
+	          "mkdir /run/mnt && "
+	          "mount \"$(findfs UUID=a7834806-2f82-4faf-8ac4-4f8fd8a474ca)\" /run/mnt && "
+	          "s6-rc -bu change vmm-env && "
+	          "vm-import user /run/mnt/vms && "
+	          "(tail -Fc +0 /run/*.log &) && "
+	          "s6-svc -O /run/vm/by-name/user.portal/service && "
+	          "vm-start \"$(basename \"$(readlink /run/vm/by-name/user.portal)\")\" && "
+	          "s6-svwait -d /run/vm/by-name/user.portal/service\n",
+	          vm.console) == EOF) {
+		fputs("error writing to console\n", stderr);
+		exit(EXIT_FAILURE);
+	}
+
+	wait_for_prompt(vm.prompt_event);
+}

release/checks/integration/default.nix
@@ -3,7 +3,7 @@
 
 import ../../../lib/call-package.nix (
 { callSpectrumPackage, src, lib, stdenv, runCommand, writeShellScript
-, clang-tools, meson, ninja, e2fsprogs, tar2ext4, libressl, qemu_kvm
+, clang-tools, meson, ninja, e2fsprogs, glib, tar2ext4, libressl, qemu_kvm
 }:
 
 let
@@ -23,12 +23,25 @@ let
     '';
   };
 
+  portalVm = callSpectrumPackage ../../../vm/make-vm.nix {} {
+    type = "nix";
+    run = writeShellScript "run" ''
+      set -x
+      ${lib.getExe' glib "gdbus"} call --session \
+          --dest org.freedesktop.portal.Desktop \
+          --object-path /org/freedesktop/portal/desktop \
+          --method org.freedesktop.portal.FileChooser.OpenFile \
+          "" "" '@a{sv} {}' || sleep inf
+    '';
+  };
+
   userData = runCommand "user-data.img" {
     nativeBuildInputs = [ e2fsprogs tar2ext4 ];
   } ''
     tar -Pcvf root.tar \
         --transform=s,^${appimage},test.appimage, ${appimage} \
-        --transform=s,^${ncVm},vms/nc, ${ncVm}
+        --transform=s,^${ncVm},vms/nc, ${ncVm} \
+        --transform=s,^${portalVm},vms/portal, ${portalVm}
     tar2ext4 -i root.tar -o $out
     tune2fs -U a7834806-2f82-4faf-8ac4-4f8fd8a474ca $out
   '';

release/checks/integration/meson.build
@@ -11,8 +11,8 @@ run_qemu = find_program('../../../scripts/run-qemu.sh')
 
 lib = static_library('spectrum-integration-test', 'lib.c')
 
-foreach test : ['appimage', 'late-serial', 'networking']
+foreach test : ['appimage', 'late-serial', 'networking', 'portal']
   test(test, executable(test, test + '.c', link_with : lib),
-    timeout : 300,
+    timeout : 400,
     args : [run_qemu])
 endforeach

commit 89f8c9238616b43e48b21e2eade41e061b006cd8

release/checks/integration: tail serial after boot
The serial files may not exist yet otherwise, so the glob will miss
them.

Fixes: f2f6d7d ("release/checks/integration: add portal test")
Signed-off-by: Alyssa Ross <hi@alyssa.is>

release/checks/integration/portal.c
@@ -18,9 +18,9 @@ void test(struct config c)
 	          "mount \"$(findfs UUID=a7834806-2f82-4faf-8ac4-4f8fd8a474ca)\" /run/mnt ; "
 	          "s6-rc -bu change vmm-env ; "
 	          "vm-import user /run/mnt/vms /run/mnt/storage ; "
-	          "tail -Fc +0 /run/vm/by-id/*/serial & "
 	          "s6-svc -O /run/vm/by-name/user.portal/service ; "
 	          "vm-start \"$(basename \"$(readlink /run/vm/by-name/user.portal)\")\" ; "
+	          "tail -Fc +0 /run/vm/by-id/*/serial & "
 	          "s6-svwait -d /run/vm/by-name/user.portal/service\n",
 	          vm_console_writer(vm)) == EOF) {
 		fputs("error writing to console\n", stderr);